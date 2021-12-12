acc614cc2f281e44bdb4efff1b4397dd714d367b
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-12-12 22:24:51 +0100

    feat(gift): added purchase logic

--- src/pages/Gift.vue
+++ src/pages/Gift.vue
@@ -1,5 +1,6 @@
 <template>
   <Header />
+  <Loading v-if="loading" />
   <Container>
     <div class="cards">
       <Card>
@@ -10,6 +11,9 @@
           @click="this.auth.login"
           >Log in through Spotify</Button
         >
+        <Button @click="initCheckout(1)"> buy 1 giftcode </Button>
+        <Button @click="initCheckout(3)"> buy 3 giftcodes </Button>
+        <Button @click="initCheckout(5)"> buy 5 giftcodes </Button>
         <Divider />
         <span class="color-text-grey"
           >Supported payment methods • All terms and conditions • Refund
@@ -31,6 +35,11 @@
         </Card>
         <Card>
           <Heading :size="1">Your coupons</Heading>
+          <Text v-if="!giftCodes">loading...</Text>
+          <Text v-if="giftCodes?.length == 0">no codes found</Text>
+          <h3 v-for="code in giftCodes" :key="code.id">
+            <pre>{{ formatCode(code.code) }}</pre>
+          </h3>
         </Card>
       </div>
     </div>
@@ -51,7 +60,7 @@
 </style>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted } from "vue";
+import { defineComponent, ref, onMounted, Ref, onBeforeMount } from "vue";
 import { useStore } from "~/store";
 
 import Container from "~/components/layout/Container.vue";
@@ -59,6 +68,7 @@ import Header from "~/components/layout/Header.vue";
 import Card from "~/components/layout/Card.vue";
 import Heading from "~/components/base/Heading.vue";
 import Divider from "~/components/base/Divider.vue";
+import Loading from "~/components/base/Loading.vue";
 import Text from "~/components/base/Text.vue";
 import Button from "~/components/base/Button.vue";
 import api from "~/api";
@@ -71,13 +81,46 @@ export default defineComponent({
     Card,
     Heading,
     Divider,
+    Loading,
     Text,
     Button,
   },
   setup() {
     const auth = useAuth();
+    const giftCodes = ref(null);
+    const loading = ref(false);
 
-    return { auth };
+    onBeforeMount(() => {
+      listGiftCodes();
+    });
+
+    const listGiftCodes = async () => {
+      giftCodes.value = await api
+        .get("/plus/giftcodes/list")
+        .then((res) => res.data.items);
+      loading.value = false;
+    };
+
+    const initCheckout = async (quantity: number) => {
+      loading.value = true;
+      const session = await api
+        .get(`/plus/giftcodes/purchase?quantity=${quantity}`)
+        .then((res) => res.data.item);
+
+      location.href = session.url;
+    };
+
+    return {
+      auth,
+      loading,
+      giftCodes,
+      initCheckout,
+    };
+  },
+  methods: {
+    formatCode(code: string) {
+      return code.match(new RegExp(".{1,4}", "g"))!.join("-");
+    },
   },
 });
 </script>
