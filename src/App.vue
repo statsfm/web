1b07ab6297950635eb71e7362cd4ef9c628d53b5
Martijn-Faber <martijn@fabtronics.nl>
2021-12-15 20:30:16 +0100

    refactor: move footer to `layout`

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 import { defineComponent } from "vue";
 
 import Header from "~/components/layout/Header.vue";
-import Footer from "~/components/base/Footer.vue";
+import Footer from "~/components/layout/Footer.vue";
 
 export default defineComponent({
   components: {
