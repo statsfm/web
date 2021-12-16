372cbf2b0cee970ef6699479f5c77d36a843d828
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-12-16 20:26:47 +0100

    feat(gift): cool price cards

--- src/pages/Gift.vue
+++ src/pages/Gift.vue
@@ -4,13 +4,50 @@
   <Container>
     <div class="flex gap-3 mb-5 flex-col md:flex-row">
       <Card>
-        <h1 class="text-2xl font-bold">Gift Plus coupons</h1>
+        <h1 class="text-4xl font-bold">Gift Plus coupons</h1>
         <Button v-if="!auth.isLoggedIn()" size="small" @click="auth.login()"
           >Log in through Spotify</Button
         >
-        <Button @click="initCheckout(1)"> buy 1 giftcode </Button>
-        <Button @click="initCheckout(3)"> buy 3 giftcodes </Button>
-        <Button @click="initCheckout(5)"> buy 5 giftcodes </Button>
+        <div class="flex flex-col md:flex-row gap-5">
+          <div class="pricing-item bg-bodyPrimary">
+            <p class="pricing-item-amount">1x</p>
+            <span class="pricing-item-description">Lifetime Plus upgrade</span>
+            <a @click="initCheckout(5)" href="#" class="pricing-item-buy">
+              <span class="pricing-item-price">4$</span>
+              <br />
+              <span class="pricing-item-fees">incl vat/fees</span>
+            </a>
+          </div>
+          <div class="pricing-item bg-bodyPrimary">
+            <p class="pricing-item-amount">3x</p>
+            <span class="pricing-item-description">Lifetime Plus upgrade</span>
+            <a @click="initCheckout(3)" href="#" class="pricing-item-buy">
+              <span class="pricing-item-price">10$</span>
+              <br />
+              <span class="pricing-item-fees">incl vat/fees</span>
+            </a>
+          </div>
+          <div class="pricing-item bg-bodyPrimary">
+            <span
+              class="
+                pricing-item-most-populair
+                bg-primary
+                text-bodyPrimary
+                font-bold
+              "
+            >
+              Most chosen
+            </span>
+            <p class="pricing-item-amount">5x</p>
+            <span class="pricing-item-description">Lifetime Plus upgrade</span>
+            <a @click="initCheckout(5)" href="#" class="pricing-item-buy">
+              <span class="pricing-item-price">15$</span>
+              <br />
+              <span class="pricing-item-fees">incl vat/fees</span>
+            </a>
+          </div>
+        </div>
+
         <Divider />
         <span class="text-textGrey"
           >Supported payment methods • All terms and conditions • Refund
@@ -70,6 +107,68 @@
     </div>
   </Container>
 </template>
+
+<style lang="scss" scoped>
+.pricing-item {
+  height: 12.6rem;
+  width: 11rem;
+  border-radius: 1rem;
+  position: relative;
+  margin: 1rem 0px;
+}
+
+.pricing-item-buy {
+  position: absolute;
+  width: 8rem;
+  text-align: center;
+  border-radius: 0.5rem;
+  bottom: -0.7rem;
+  left: calc(11rem / 2 - 8rem / 2);
+  background-color: rgb(26, 32, 24);
+}
+
+.pricing-item-price {
+  text-align: center;
+  font-size: 1.8em;
+  font-weight: bold;
+  line-height: 1;
+  position: relative;
+  bottom: -5px;
+}
+.pricing-item-fees {
+  font-size: 0.8em;
+  text-align: center;
+  color: rgb(119, 112, 112);
+  line-height: 1;
+}
+.pricing-item-amount {
+  font-size: 4rem;
+  text-align: center;
+  position: absolute;
+  width: 100%;
+  font-weight: bold;
+  top: 0.5rem;
+}
+
+.pricing-item-description {
+  font-size: 1.3rem;
+  text-align: center;
+  position: absolute;
+  width: 100%;
+  font-weight: bold;
+  top: 6rem;
+  line-height: 1;
+  color: rgb(119, 112, 112);
+}
+.pricing-item-most-populair {
+  position: absolute;
+  width: 7.5rem;
+  text-align: center;
+  border-radius: 0.5rem;
+  top: -0.7rem;
+  left: calc(11rem / 2 - 7.5rem / 2);
+}
+</style>
 
 <script lang="ts">
 import { defineComponent, ref, Ref, onBeforeMount } from "vue";
