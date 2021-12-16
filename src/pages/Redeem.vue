3e31cba25641ff36db49ee457375e2e9061ca6ff
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-12-16 20:24:54 +0100

    feat(redeem): pin input for redeem

--- src/pages/Redeem.vue
+++ src/pages/Redeem.vue
@@ -1,20 +1,32 @@
 <template>
-  <Header />
   <Loading v-if="loading" />
   <Container>
-    <Heading :size="1">Redeem a Plus coupon</Heading>
     <br />
-    <input
-      class="code-input"
-      type="text"
-      name="code"
-      pattern="[A-Z,a-z,0-9]{12}"
-      maxlength="12"
-      v-model="code"
-    />
-    <PinInput />
+    <h1 class="text-4xl font-bold">Redeem a Plus coupon</h1>
     <br />
-    <br />
+    <div class="flex gap-1 mb-5 flex-col md:flex-row">
+      <div v-for="i in pinLength" :key="i" class="w-100/10">
+        <input
+          class="pin-input font-bold"
+          type="text"
+          name="code"
+          pattern="[A-Z,a-z,0-9]"
+          maxlength="1"
+          v-model="pinInput[i - 1]"
+          :readonly="i - 1 != pinNextIndex"
+          :id="'pin' + (i - 1)"
+          @click="i - 1 != pinNextIndex ? pinFocus() : () => {}"
+          :onkeydown="(e) => pinKeypress(e, i - 1)"
+        />
+        <h1
+          class="text-2xl font-bold"
+          v-if="i % 4 == 0 && i != pinLength"
+          style="display: inline"
+        >
+          -
+        </h1>
+      </div>
+    </div>
     <Text v-if="message">{{ message }}</Text>
     <br />
     <Button :disabled="code.length != 12" @click="submitCode">Redeem</Button>
@@ -22,6 +34,19 @@
 </template>
 
 <style lang="scss" scoped>
+.pin-input {
+  border: none;
+  background-color: var(--color-body-secundary);
+  color: var(--color-text-primary);
+  font-size: 1.8rem;
+  letter-spacing: 0px;
+  text-transform: uppercase;
+  font-family: "Open Sans Bold", sans-serif;
+  border-radius: 12px;
+  padding: 5px 10px;
+  width: 3rem;
+  text-align: center;
+}
 .code-input {
   border: none;
   background-color: var(--color-body-secundary);
@@ -75,19 +100,58 @@ export default defineComponent({
     const loading = ref(false);
     const code = ref("");
     const message = ref("");
-    // const formattedCode = computed({
-    //   get: () => {
-    //     if (code.value.length == 0) return "";
-    //     return code.value
-    //       .split("-")
-    //       .join("")
-    //       .match(new RegExp(".{1,4}", "g"))!
-    //       .join("-");
-    //   },
-    //   set: (val) => {
-    //     code.value = val;
-    //   },
-    // });
+    const pinLength = 12;
+    const pinInputRef = ref(new Array(pinLength).fill(""));
+    const pinNextIndex = ref(0);
+    const formattedCode = computed({
+      get: () => {
+        if (pinInputRef.value.join("").length == 0) return "";
+        return pinInputRef.value
+          .join("")
+          .match(new RegExp(".{1,4}", "g"))!
+          .join("-");
+      },
+      set: (val) => {},
+    });
+    const pinInput = computed({
+      get: () => {
+        return pinInputRef.value;
+      },
+      set: (val) => {
+        pinInputRef.value = val;
+      },
+    });
+
+    const pinFocus = () => {
+      document.getElementById(`pin${pinNextIndex.value}`)?.focus();
+    };
+    const pinKeypress = (e, pin) => {
+      if (e.which == 8) {
+        if (pin == pinLength - 1) {
+          if (pinInput.value[pin] == "") {
+            if (pinNextIndex.value != 0) pinNextIndex.value--;
+            pinInput.value[pin - 1] = "";
+          }
+        } else {
+          if (pinNextIndex.value != 0) pinNextIndex.value--;
+          pinInput.value[pin - 1] = "";
+        }
+        pinInput.value[pin] = "";
+        pinFocus();
+      } else if (e.which == 13 && pinInput.value[pin] != "") {
+        pinNextIndex.value++;
+        pinFocus();
+      }
+    };
+
+    watch(pinInput.value, () => {
+      if (pinNextIndex.value != pinLength - 1) {
+        pinNextIndex.value = pinInput.value.findIndex((val) => val == "");
+      }
+
+      // document.getElementById(`pin${nextIndex}`)?.removeAttribute("readonly");
+      pinFocus();
+    });
 
     onBeforeMount(() => {});
 
@@ -110,7 +174,12 @@ export default defineComponent({
       code,
       message,
       submitCode,
-      //   formattedCode,
+      pinLength,
+      pinInput,
+      pinNextIndex,
+      pinFocus,
+      pinKeypress,
+      formattedCode,
     };
   },
 });
