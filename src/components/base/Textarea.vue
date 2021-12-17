ae9546827ee86d0ac82b71f2b0dc579ac71d4420
Martijn-Faber <martijn@fabtronics.nl>
2021-12-17 21:03:46 +0100

    feat(textarea): add the `Textarea` component

---
+++ src/components/base/Textarea.vue
@@ -0,0 +1,32 @@
+<template>
+  <textarea
+    @input="(e: any) => $emit('input', e.target.value)"
+    :value="value"
+    :style="{ resize: isResizeable ? 'auto' : 'none' }"
+    :placeholder="placeholder"
+    class="bg-bodySecundary rounded-2xl p-5 w-full focus:outline-none"
+  ></textarea>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    placeholder: {
+      type: String,
+      required: false,
+    },
+    isResizeable: {
+      type: Boolean,
+      default: true,
+      required: false,
+    },
+    value: {
+      type: String,
+      required: false,
+    },
+  },
+  emits: ["input"],
+});
+</script>
