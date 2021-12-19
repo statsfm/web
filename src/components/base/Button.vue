c28a3eff4947df03c5235b67339842b36c7d62e7
Martijn-Faber <martijn@fabtronics.nl>
2021-12-19 19:06:46 +0100

    fix(button): inherit `active` and `hover` state and add the button disabled option

--- src/components/base/Button.vue
+++ src/components/base/Button.vue
@@ -13,8 +13,10 @@
         type === 'primary',
       'py-2 px-5 rounded-xl': size === 'small',
       'py-3 px-5 rounded-2xl': size === 'medium',
-      'bg-[#727272]/25 text-[#727272]': disabled,
+      'bg-[#727272]/25 text-[#727272] hover:bg-[#727272]/25 active:bg-[#727272]/25':
+        disabled,
     }"
+    :disabled="disabled"
     @click="(e) => $emit('click', e)"
   >
     <slot />
