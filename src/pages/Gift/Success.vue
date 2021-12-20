6b4e32a712da147328ff77c0deca5506c552c131
Martijn-Faber <martijn@fabtronics.nl>
2021-12-20 19:14:14 +0100

    feat(success): add a simple success page

---
+++ src/pages/Gift/Success.vue
@@ -0,0 +1,55 @@
+<template>
+  <Container class="h-screen">
+    <div class="mt-20 grid place-items-center text-center">
+      <img
+        :src="gif"
+        class="rounded-full w-48 aspect-square mb-5 bg-center bg-contain"
+      />
+      <h1 class="text-3xl font-bold md:text-4xl">
+        {{ t("gift.thanks_plus") }}
+      </h1>
+      <p class="text-textGrey font-bold mt-2 max-w-prose">
+        {{ t("gift.order_info") }}
+      </p>
+      <Button class="max-w-fit mt-5" @click="$router.push({ name: 'Gift' })">{{
+        t("gift.take_to_coupons_page")
+      }}</Button>
+    </div>
+  </Container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useI18n } from "vue-i18n";
+
+import Container from "~/components/layout/Container.vue";
+import Button from "~/components/base/Button.vue";
+
+export default defineComponent({
+  components: {
+    Container,
+    Button,
+  },
+  setup() {
+    const { t } = useI18n();
+
+    const gifs = [
+      "https://c.tenor.com/BnEKiDKJisEAAAAC/claire-dancing.gif",
+      "https://c.tenor.com/4-C47Bn_MUgAAAAd/friday-happy-dance.gif",
+      "https://c.tenor.com/2_6lWnksyaUAAAAd/dancing-dirty.gif",
+      "https://c.tenor.com/cKf7FeLja6cAAAAC/cant-wait-excited.gif",
+      "https://c.tenor.com/uFy_1B10El0AAAAC/aye-ouu.gif",
+      "https://c.tenor.com/LzFmt_ca1AEAAAAd/aye-aye-aye-turnup.gif",
+      "https://c.tenor.com/1vzjD25uEjcAAAAC/clapping-yay.gif",
+      "https://c.tenor.com/PXVgzio7BmcAAAAC/happy-dance.gif",
+      "https://c.tenor.com/xgDmUszn5PYAAAAd/clapping-happy.gif",
+      "https://c.tenor.com/Hrzj4EjdX58AAAAd/dance-happy.gif",
+      "https://media2.giphy.com/media/26DOpJRJMhRPGqUsE/giphy.gif",
+      "https://media0.giphy.com/media/3o85g2cRAMwUTuPfGM/giphy.gif",
+    ];
+
+    const gif = gifs[Math.floor(Math.random() * gifs.length)];
+    return { t, gif };
+  },
+});
+</script>
