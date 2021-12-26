1c9189441da227b58a698ae68afcfc220376a6af
Martijn-Faber <martijn@fabtronics.nl>
2021-12-26 19:19:27 +0100

    refactor(Modal): rewrite `Modal` to use tailwind

--- src/components/base/Modals/Modal.vue
+++ src/components/base/Modals/Modal.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="overlay" @click="$emit('hide')">
+  <div
+    class="
+      fixed
+      top-0
+      left-0
+      h-screen
+      w-screen
+      bg-black/40
+      grid
+      place-items-center
+      cursor-pointer
+      animation-fade
+      z-50
+    "
+    @click="$emit('hide')"
+  >
     <div
       class="
         bg-bodySecundary
@@ -18,32 +33,7 @@
   </div>
 </template>
 
-<style lang="scss" scoped>
-.overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: grid;
-  place-items: center;
-  width: 100vw;
-  height: 100vh;
-  background: rgba(0, 0, 0, 0.3);
-  z-index: 10000;
-  animation-duration: 0.3s;
-  animation-fill-mode: both;
-  animation-name: fade;
-  cursor: pointer;
-}
-</style>
-
 <script lang="ts" setup>
 import Container from "~/components/layout/Container.vue";
-
-const emit = defineEmits(["hide"]);
-
-const hide = (e: Event) => {
-  emit("hide", e);
-};
+defineEmits(["hide"]);
 </script>
