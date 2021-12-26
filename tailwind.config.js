6ed3afc2e59dc93cc539ab81c61daf530643c82c
Martijn-Faber <martijn@fabtronics.nl>
2021-12-26 19:20:06 +0100

    fix(tailwind): move colors to `extend` so you we can use the default tailwind colors too

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      primary: "#1ed760",
-      primaryLighter: "rgba(30, 215, 96, 0.1)",
-      bodyPrimary: "#111112",
-      bodySecundary: "#18181c",
-      textGrey: "#727272",
-    },
     extend: {
+      colors: {
+        primary: "#1ed760",
+        primaryLighter: "rgba(30, 215, 96, 0.1)",
+        bodyPrimary: "#111112",
+        bodySecundary: "#18181c",
+        textGrey: "#727272",
+      },
       keyframes: {
         fade: {
           from: { opacity: 0 },
