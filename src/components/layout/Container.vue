b43ef27331028c693db17c6187193bd376928e3c
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-12-27 23:56:22 +0100

    feat(container): altered container width

--- src/components/layout/Container.vue
+++ src/components/layout/Container.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="container">
+  <div
+    class="
+      mx-auto
+      max-w-md
+      px-4
+      sm:max-w-3xl sm:px-6
+      lg:px-8 lg:max-w-7xl
+      relative
+    "
+  >
     <slot />
   </div>
 </template>
