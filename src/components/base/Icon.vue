9f3bf5175c7f4ff673ed1679634f4d705e479e65
Martijn-Faber <martijn@fabtronics.nl>
2021-12-27 21:23:46 +0100

    feat(Icon): add a `Icon` component which renders svg paths

---
+++ src/components/base/Icon.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewport="0 0 24 24"
+    :style="{ fill: color ?? 'currentColor' }"
+  >
+    <path :d="path" />
+  </svg>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  path: string;
+  color?: string;
+}>();
+</script>
