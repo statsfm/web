3dcc1d6db388aee9910bf4ae1e1671e0834c8c38
Martijn-Faber <martijn@fabtronics.nl>
2021-12-28 11:27:54 +0100

    feat(RedeemCode): route back to the `Redeem` page if giftcode is not found

--- src/pages/Redeem/RedeemCode.vue
+++ src/pages/Redeem/RedeemCode.vue
@@ -1,8 +1,8 @@
 <template>
   <Header />
   <LoadingOverlay v-if="isLoading" />
-  <Container class="h-screen flex flex-col items-center">
-    <GiftCard v-if="giftCode" :giftCode="giftCode" :isFlipped="isFlipped" class="mt-24" />
+  <Container class="h-screen flex flex-col items-center" v-if="giftCode">
+    <GiftCard :giftCode="giftCode" :isFlipped="isFlipped" class="mt-24" />
     <Button class="max-w-xl" @click="redeemGiftCode">{{ t('buttons.redeem') }}</Button>
   </Container>
 </template>
@@ -15,7 +15,7 @@ import Container from '~/components/layout/Container.vue';
 import GiftCard from '~/components/base/GiftCard.vue';
 import { GiftCode } from '~/types';
 import api from '~/api';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import Button from '~/components/base/Button.vue';
 import { useI18n } from 'vue-i18n';
 import { useStore } from '~/store';
@@ -23,6 +23,7 @@ import LoadingOverlay from '~/components/base/LoadingOverlay.vue';
 
 const { t } = useI18n();
 const store = useStore();
+const router = useRouter();
 const route = useRoute();
 
 const giftCode: Ref<GiftCode | null> = ref(null);
@@ -34,31 +35,38 @@ const getGiftCode = async (code: string) => {
   isLoading.value = true;
   const res = await api.get(`/plus/giftcodes/${code}`);
 
-  if (res.success) {
-    giftCode.value = res.data.item;
-  } else {
+  if (!res.success) {
+    // route back to the redeem page if gift code isn't found
+    if (res.status == 404) {
+      router.push({ name: 'Redeem' });
+    }
+
     store.commit('setError', { message: res.data.message, type: 'error' });
+    return;
   }
 
+  giftCode.value = res.data.item;
   isLoading.value = false;
 };
 
 const redeemGiftCode = async () => {
   const res = await api.post(`/plus/giftcodes/${code.value}/redeem`);
 
-  if (res.success) {
-    store.commit('setError', {
-      message: t('errors.successfully_redeemed'),
-      type: 'info'
-    });
-
-    isFlipped.value = true;
-  } else {
+  if (!res.success) {
     store.commit('setError', {
       message: res.data.message,
       type: 'error'
     });
+
+    return;
   }
+
+  store.commit('setError', {
+    message: t('errors.successfully_redeemed'),
+    type: 'info'
+  });
+
+  isFlipped.value = true;
 };
 
 onMounted(() => {
