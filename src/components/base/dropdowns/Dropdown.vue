bfe96a4b548f38f7c17c9f25b6f142fe83f85e91
Martijn-Faber <martijn@fabtronics.nl>
2021-12-29 00:24:13 +0100

    refactor(Dropdown): rewrite to tailwind

--- src/components/base/dropdowns/Dropdown.vue
+++ src/components/base/dropdowns/Dropdown.vue
@@ -1,46 +1,21 @@
 <template>
-  <div class="dropdown">
+  <div class="relative">
     <div @click="toggle">
       <slot name="button" />
     </div>
 
+    <!-- TODO: fix animations -->
     <transition name="dropdown">
-      <Card class="content" v-if="isDropdownActive" v-click-away="onClickAway">
+      <Card
+        class="mt-2 absolute w-max h-max right-0 shadow-xl z-50"
+        v-if="isDropdownActive"
+        v-click-away="onClickAway"
+      >
         <slot />
       </Card>
     </transition>
   </div>
 </template>
-
-<style lang="scss" scoped>
-.dropdown {
-  position: relative;
-}
-
-.content {
-  padding: var(--space-s);
-  background-color: var(--color-body-secundary);
-  position: absolute;
-  width: max-content;
-  max-width: 350px;
-  height: max-content;
-  right: 0px;
-  box-shadow: var(--shadow-xs);
-  z-index: 1000;
-  margin-top: var(--space-s);
-  display: flex;
-  flex-direction: column;
-  transition: scale 0.2s;
-  animation-name: fade-in;
-  animation-duration: 0.2s;
-}
-
-.dropdown-leave-to {
-  animation-name: fade-out;
-  animation-duration: 0.2s;
-  scale: 0.95;
-}
-</style>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
