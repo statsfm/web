db24d09b7bbc49617c540f2f846aaf7718e2d06b
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-12-28 01:29:58 +0100

    feat(privacy): added privacy page

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,11 @@ const routes: RouteRecordRaw[] = [
     component: () => import("../pages/Home.vue"),
   },
   {
+    path: "/privacy",
+    name: "Privacy Policy",
+    component: () => import("../pages/Privacy.vue"),
+  },
+  {
     path: "/auth:route(.*)",
     name: "AuthCallback",
     component: () => import("../pages/Auth/AuthCallback.vue"),
