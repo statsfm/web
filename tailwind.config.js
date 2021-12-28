cd848d159dc64d4ccb4fe82cadb7c587c527474b
Martijn-Faber <martijn@fabtronics.nl>
2021-12-28 15:14:55 +0100

    feat(tailwind): add the `typography` plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        primary: "#1ed760",
-        primaryLighter: "rgba(30, 215, 96, 0.1)",
-        bodyPrimary: "#111112",
-        bodySecundary: "#18181c",
-        textGrey: "#727272",
+        primary: '#1ed760',
+        primaryLighter: 'rgba(30, 215, 96, 0.1)',
+        bodyPrimary: '#111112',
+        bodySecundary: '#18181c',
+        textGrey: '#727272'
       },
       keyframes: {
         fade: {
           from: { opacity: 0 },
-          to: { opacity: 1 },
-        },
+          to: { opacity: 1 }
+        }
       },
       animation: {
-        fade: "fade 0.3s ease-in-out",
-      },
+        fade: 'fade 0.3s ease-in-out'
+      }
     },
     container: {
       center: true,
       padding: {
-        DEFAULT: "1.5rem",
-      },
-    },
+        DEFAULT: '1.5rem'
+      }
+    }
   },
-  plugins: [require("@tailwindcss/line-clamp")],
+  plugins: [require('@tailwindcss/line-clamp'), require('@tailwindcss/typography')]
 };
