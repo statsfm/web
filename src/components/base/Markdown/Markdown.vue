7ae4187ded0701b5f76f4c1494ec3ea7b5b92003
Martijn-Faber <martijn@fabtronics.nl>
2021-12-28 15:15:57 +0100

    feat(Markdown): add a component that renders markdown and a component that takes a source of a markdown file and fetches it to render

---
+++ src/components/base/Markdown/Markdown.vue
@@ -0,0 +1,19 @@
+<template>
+  <MarkdownRender :markdown="markdown" />
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+
+import MarkdownRender from './MarkdownRender.vue';
+
+const props = defineProps<{
+  src: string;
+}>();
+
+const markdown = ref('');
+
+onMounted(async () => {
+  markdown.value = await fetch(props.src).then((res) => res.text());
+});
+</script>
---
+++ src/components/base/Markdown/MarkdownRender.vue
@@ -0,0 +1,14 @@
+<template>
+  <article
+    class="prose max-w-prose prose-headings:text-white prose-a:text-primary prose-p:text-gray-500 font-medium prose-li:text-gray-500"
+    v-html="marked(markdown)"
+  />
+</template>
+
+<script lang="ts" setup>
+import { marked } from 'marked';
+
+defineProps<{
+  markdown: string;
+}>();
+</script>
