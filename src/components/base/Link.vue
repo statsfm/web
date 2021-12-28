dfc602aa9b21765569fbee85eb481bedd00cd4da
Martijn-Faber <martijn@fabtronics.nl>
2021-12-28 15:43:23 +0100

    feat(Link): add the link component

---
+++ src/components/base/Link.vue
@@ -0,0 +1,12 @@
+<template>
+  <a v-if="typeof to == 'string' && to.startsWith('http')" :href="to"><slot /></a>
+  <router-link v-else :to="to"><slot /></router-link>
+</template>
+
+<script lang="ts" setup>
+import { LocationAsRelativeRaw } from 'vue-router';
+
+defineProps<{
+  to: string | LocationAsRelativeRaw;
+}>();
+</script>
