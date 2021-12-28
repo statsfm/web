979cc7075c11075c75e024a18cc664c1e52f80a7
Martijn-Faber <martijn@fabtronics.nl>
2021-12-28 17:13:13 +0100

    improvement(Modal): use a `Teleport` for the modal and use click away

--- src/components/base/Modals/Modal.vue
+++ src/components/base/Modals/Modal.vue
@@ -1,15 +1,22 @@
 <template>
-  <div
-    class="fixed top-0 left-0 h-screen w-screen bg-black/40 grid place-items-center cursor-pointer animation-fade z-50"
-    @click="$emit('hide')"
-  >
-    <div class="bg-bodySecundary rounded-2xl max-h-max p-5 shadow-xl">
-      <slot />
+  <Teleport to="body">
+    <div
+      class="fixed top-0 left-0 h-screen w-screen bg-black/40 grid place-items-center cursor-pointer animation-fade z-40"
+    >
+      <div
+        class="bg-bodySecundary rounded-2xl max-h-max p-5 shadow-xl z-50 cursor-default"
+        v-click-away="onClickAway"
+      >
+        <slot />
+      </div>
     </div>
-  </div>
+  </Teleport>
 </template>
 
 <script lang="ts" setup>
-import Container from '~/components/layout/Container.vue';
-defineEmits(['hide']);
+const emit = defineEmits(['hide']);
+
+const onClickAway = () => {
+  emit('hide');
+};
 </script>
