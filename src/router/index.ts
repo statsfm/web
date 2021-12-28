0d6bd34f076401c09f9416540831ac85c00650b3
Martijn-Faber <martijn@fabtronics.nl>
2021-12-28 11:12:00 +0100

    feat(router): show the progress bar while loading the route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,4 @@
+import NProgress from 'nprogress';
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
@@ -64,7 +65,21 @@ const routes: RouteRecordRaw[] = [
   }
 ];
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes
 });
+
+NProgress.configure({
+  template: '<div class="fixed z-59 top-0 left-0 w-full h-0.5 bg-primary" role="bar"></div>'
+});
+
+router.beforeEach(() => {
+  NProgress.start();
+});
+
+router.afterEach(() => {
+  NProgress.done();
+});
+
+export default router;
