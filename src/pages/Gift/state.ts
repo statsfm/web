77642c16a92de38c80a24881c445c7cb16df1b5b
Martijn-Faber <martijn@fabtronics.nl>
2021-12-29 18:02:40 +0100

    feat(Gift): use shared `giftCodes` state

--- src/pages/Gift/state.ts
+++ src/pages/Gift/state.ts
@@ -1,4 +1,4 @@
-import { ref, Ref } from "vue";
-import { GiftCode } from "~/types";
+import { ref, Ref } from 'vue';
+import { GiftCode } from '~/types';
 
 export const giftCodes: Ref<GiftCode[] | null> = ref(null);
