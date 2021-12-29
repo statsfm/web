452200dc20efcb8993d14a5664a39e7816fbb5c4
Martijn-Faber <martijn@fabtronics.nl>
2021-12-29 20:01:42 +0100

    fix(FeatureItem): add the `FeatureItem` component

---
+++ src/components/base/FeatureItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex items-start gap-3">
+    <div
+      class="text-primary bg-primaryLighter min-w-[3rem] aspect-square rounded-full font-bold text-xl grid place-items-center"
+    >
+      <Icon :path="icon" color="#1ed760" />
+    </div>
+    <div class="flex flex-col align-middle">
+      <h2 class="text-2xl font-bold">{{ title }}</h2>
+      <p><slot /></p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Icon from './Icon.vue';
+
+defineProps<{
+  icon: string;
+  title: string;
+}>();
+</script>
