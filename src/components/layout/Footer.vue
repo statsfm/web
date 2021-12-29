041fb6d56a310d274beab7fa371ec4c5017fdafc
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-12-29 19:26:12 +0100

    feat(footer): refactored footer

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,55 +1,129 @@
 <template>
-  <Container class="pt-14 pb-14 flex flex-col flex-wrap">
-    <div class="font-bold grid grid-cols-3 md:grid-cols-5 mb-6">
-      <div class="flex flex-col gap-2">
-        <p class="text-textGrey">Spotistats</p>
-        <router-link :to="{ name: 'Home' }">Home</router-link>
-        <a href="https://support.spotistats.app/" target="blank">Support</a>
-        <a>About</a>
-      </div>
-
-      <div class="hidden md:flex flex-col gap-2">
-        <p class="text-textGrey">Socials</p>
-        <a href="https://twitter.com/spotistats" target="blank">Twitter</a>
-        <a href="https://discord.com/invite/aV9EtB3" target="blank">Discord</a>
-        <a href="https://www.instagram.com/spotistats" target="blank">Instagram</a>
-      </div>
-
-      <!-- <div class="flex flex-col gap-2">
-        <p class="text-textGrey">Explore</p>
-        <a>Artists</a>
-        <a>Tracks</a>
-        <a>Albums</a>
-      </div> -->
-
-      <div class="flex flex-col gap-2">
-        <p class="text-textGrey">Legal</p>
-        <router-link :to="{ name: 'Privacy' }">Privacy</router-link>
-        <router-link :to="{ name: 'Terms' }">Terms</router-link>
-      </div>
-
-      <div class="flex flex-col gap-2 mt-5 md:mt-0">
-        <p class="text-textGrey">Download</p>
-        <div class="flex gap-2 justify-center w-full">
-          <a
-            class="hover:scale-[1.02] h-6/12"
-            href="https://apps.apple.com/app/spotistats-for-spotify/id1526912392"
-            target="blank"
-            ><img src="../../assets/apple_appstore_badge.png" alt="Apple Appstore" />
-          </a>
-          <a
-            class="hover:scale-[1.02] h-6/12"
-            href="https://play.google.com/store/apps/details?id=dev.netlob.spotistats"
-            target="blank"
-            ><img src="../../assets/google_play_badge.png" alt="Google Play" />
-          </a>
+  <Container>
+    <div class="xl:grid xl:gap-8 font-bold">
+      <div class="grid grid-cols-2 gap-8 xl:col-span-2">
+        <div class="md:grid md:grid-cols-2 md:gap-8">
+          <div>
+            <h3 class="text-md font-bold text-textGrey tracking-wider">Backtrack</h3>
+            <ul class="mt-4 space-y-4">
+              <li>
+                <router-link class="text-base text-gray-300 hover:text-white" :to="{ name: 'Home' }"
+                  >Home</router-link
+                >
+              </li>
+              <li>
+                <a
+                  class="text-base text-gray-300 hover:text-white"
+                  href="https://support.spotistats.app/"
+                  target="blank"
+                  >Support</a
+                >
+              </li>
+              <li>
+                <a
+                  class="text-base text-gray-300 hover:text-white"
+                  href="https://discord.gg/NeaBE96Vs6"
+                  target="_blank"
+                  >Discord</a
+                >
+              </li>
+            </ul>
+          </div>
+          <div class="mt-12 md:mt-0">
+            <h3 class="text-md font-bold text-textGrey tracking-wider">Socials</h3>
+            <ul class="mt-4 space-y-4">
+              <li>
+                <a
+                  class="text-base text-gray-300 hover:text-white"
+                  href="https://discord.gg/backtrack"
+                  target="blank"
+                  >Discord</a
+                >
+              </li>
+              <li>
+                <a
+                  class="text-base text-gray-300 hover:text-white"
+                  href="https://twitter.com/spotistats"
+                  target="blank"
+                  >Twitter</a
+                >
+              </li>
+              <li>
+                <a
+                  class="text-base text-gray-300 hover:text-white"
+                  href="https://www.instagram.com/spotistats"
+                  target="blank"
+                  >Instagram</a
+                >
+              </li>
+            </ul>
+          </div>
         </div>
-        <Button class="hidden md:block">Open webapp</Button>
+        <div class="md:grid md:grid-cols-2 md:gap-8">
+          <div class="mt-12 md:mt-0">
+            <h3 class="text-md font-bold text-textGrey tracking-wider">Legal</h3>
+            <ul class="mt-4 space-y-4">
+              <li>
+                <router-link
+                  class="text-base text-gray-300 hover:text-white"
+                  :to="{ name: 'Privacy' }"
+                  >Privacy</router-link
+                >
+              </li>
+              <li>
+                <router-link
+                  class="text-base text-gray-300 hover:text-white"
+                  :to="{ name: 'Terms' }"
+                  >Terms</router-link
+                >
+              </li>
+            </ul>
+          </div>
+          <div class="mt-10 md:mt-0">
+            <h3 class="text-md font-semibold text-textGrey tracking-wider">Download</h3>
+            <div class="mt-4"></div>
+            <a
+              class="h-12"
+              href="https://apps.apple.com/app/spotistats-for-spotify/id1526912392"
+              target="blank"
+              ><img src="../../assets/apple_appstore_badge.png" alt="Apple Appstore" class="h-12" />
+            </a>
+            <div class="mt-2"></div>
+            <a
+              class="h-12"
+              href="https://play.google.com/store/apps/details?id=dev.netlob.spotistats"
+              target="blank"
+              ><img src="../../assets/google_play_badge.png" alt="Google Play" class="h-12" />
+            </a>
+            <div class="mt-2">
+              <Button class="hidden md:block">Open webapp</Button>
+            </div>
+            <!-- <h3 class="text-md font-bold text-textGrey tracking-wider">Explore</h3>
+            <ul class="mt-4 space-y-4">
+              <li>
+                <a
+                  href="https://twitter.com/hiettdigital"
+                  target="_blank"
+                  class="text-base text-gray-300 hover:text-white"
+                  >@hiettdigital</a
+                >
+              </li>
+              <li>
+                <a
+                  href="https://twitter.com/roberttwestbury"
+                  target="_blank"
+                  class="text-base text-gray-300 hover:text-white"
+                  >@roberttwestbury</a
+                >
+              </li>
+            </ul> -->
+          </div>
+        </div>
       </div>
     </div>
-    <span class="text-textGrey font-bold"
-      >@ 2021 Spotistats. All rights reserverd. Made with ❤️ in the Netherlands</span
-    >
+    <p class="mt-10 pt-10 text-center text-textGrey font-bold md:mt-0 md:order-1">
+      © 2021 Backtrack / Spotistats. All rights reserved. Made with ❤️ in the Netherlands
+    </p>
   </Container>
 </template>
 
