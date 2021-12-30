6b516e32cbd4c138ec3ba4f073d65d99419402b9
Martijn-Faber <martijn@fabtronics.nl>
2021-12-30 09:47:19 +0100

    fix(TotalStats): remove the placeholder data and replace it with the api

--- src/components/base/TotalStats/TotalStats.vue
+++ src/components/base/TotalStats/TotalStats.vue
@@ -12,78 +12,18 @@
 
 <script lang="ts" setup>
 import { onMounted, ref, Ref } from 'vue';
+import api from '~/api';
 import { TotalSize } from '~/types/totalStats';
 import TotalStatsItem from './TotalStatsItem.vue';
 
 const stats: Ref<TotalSize | null> = ref(null);
 
 const getStats = async (): Promise<TotalSize> => {
-  // return await api.get("/stats/database/size").then((res) => res.data);
-  return {
-    users: {
-      current: {
-        count: 2196279,
-        date: '2021-12-29T01:04:22+0000'
-      },
-      previous: {
-        count: 2189149,
-        date: '2021-12-28T01:04:22+0000'
-      }
-    },
-    plusUsers: {
-      current: {
-        count: 85451,
-        date: '2021-12-29T01:04:22+0000'
-      },
-      previous: {
-        count: 85751,
-        date: '2021-12-28T01:04:22+0000'
-      }
-    },
-    streams: {
-      current: {
-        count: 1193989669,
-        date: '2021-12-29T01:04:22+0000'
-      },
-      previous: {
-        count: 1191796851,
-        date: '2021-12-28T01:04:22+0000'
-      }
-    },
-    tracks: {
-      current: {
-        count: 33523551,
-        date: '2021-12-29T01:04:22+0000'
-      },
-      previous: {
-        count: 33456774,
-        date: '2021-12-28T01:04:22+0000'
-      }
-    },
-    artists: {
-      current: {
-        count: 2639886,
-        date: '2021-12-29T01:04:22+0000'
-      },
-      previous: {
-        count: 2634187,
-        date: '2021-12-28T01:04:22+0000'
-      }
-    },
-    albums: {
-      current: {
-        count: 4666796,
-        date: '2021-12-29T01:04:22+0000'
-      },
-      previous: {
-        count: 4656270,
-        date: '2021-12-28T01:04:22+0000'
-      }
-    }
-  };
+  return await api.get('/stats/database/size').then((res) => res.data.items);
 };
 
 onMounted(async () => {
   stats.value = await getStats();
+  console.log(stats.value);
 });
 </script>
