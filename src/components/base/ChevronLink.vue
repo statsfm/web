5b30aecd4e188192e71d90cdf72df4e807d46947
Martijn-Faber <martijn@fabtronics.nl>
2021-12-31 19:13:29 +0100

    feat(ChevronLink): add the `ChervonLink` component

---
+++ src/components/base/ChevronLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <Link :to="to" class="text-primary font-bold flex items-center"
+    ><slot /><Icon :path="mdiChevronRight"
+  /></Link>
+</template>
+
+<script lang="ts" setup>
+import Link from './Link.vue';
+import Icon from './Icon.vue';
+import { mdiChevronRight } from '@mdi/js';
+import { LocationAsRelativeRaw } from 'vue-router';
+
+defineProps<{
+  to: string | LocationAsRelativeRaw;
+}>();
+</script>
