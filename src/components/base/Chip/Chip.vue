1be6519a34095647fa4547776f155fc4936effbf
Martijn-Faber <martijn@fabtronics.nl>
2021-12-31 20:44:55 +0100

    fix(Chip): set min width to max content so the chip stays on one line

--- src/components/base/Chip/Chip.vue
+++ src/components/base/Chip/Chip.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-bodySecundary rounded-full h-max px-4 py-2 font-medium">
+  <div class="bg-bodySecundary rounded-full h-max min-w-max px-4 py-2 font-medium">
     <slot />
   </div>
 </template>
