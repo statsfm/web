f53f101c256d329492dfaea95ffd09f700e20b93
Martijn-Faber <martijn@fabtronics.nl>
2022-01-01 15:57:27 +0100

    fix(Link): add some styles to the link component

--- src/components/base/Link.vue
+++ src/components/base/Link.vue
@@ -1,6 +1,14 @@
 <template>
-  <a v-if="typeof to == 'string' && to.startsWith('http')" :href="to" class="min-w-max"><slot /></a>
-  <router-link v-else :to="to" class="min-w-max"><slot /></router-link>
+  <a
+    v-if="typeof to == 'string' && to.startsWith('http')"
+    :href="to"
+    target="blank"
+    class="min-w-max text-base text-white hover:text-gray-300"
+    ><slot
+  /></a>
+  <router-link v-else :to="to" class="min-w-max text-base text-white hover:text-gray-300"
+    ><slot
+  /></router-link>
 </template>
 
 <script lang="ts" setup>
