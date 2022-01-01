7e939bdaed80f0156193574d437fd0c93df0422b
Martijn-Faber <martijn@fabtronics.nl>
2022-01-01 15:16:48 +0100

    feat(ChipGroup): add horizontal scrolling by trackpaad or scrollwheel

--- src/components/base/Chip/ChipGroup.vue
+++ src/components/base/Chip/ChipGroup.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="flex gap-2 flex-nowrap md:flex-wrap overflow-x-auto">
+  <div
+    class="flex gap-2 flex-nowrap overflow-x-auto"
+    @wheel.prevent="onWheelScroll"
+    ref="chipgroup"
+  >
     <slot />
   </div>
 </template>
+
+<script lang="ts" setup>
+import { Ref, ref } from 'vue';
+
+const chipgroup: Ref<HTMLDivElement | undefined> = ref();
+
+const onWheelScroll = (e: WheelEvent) => {
+  if (chipgroup.value) {
+    chipgroup.value.scrollLeft += e.deltaY || e.deltaX; // deltaX for trackpads
+  }
+};
+</script>
