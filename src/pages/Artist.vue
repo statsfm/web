0b672dbe299e18661828df928ebceba8fb9b342a
Martijn-Faber <martijn@fabtronics.nl>
2022-01-01 16:03:01 +0100

    feat(Artist): use the `HeroProfile` component and wrap the `Chip` in the `Link` component

--- src/pages/Artist.vue
+++ src/pages/Artist.vue
@@ -1,27 +1,21 @@
 <template>
-  <Header class="bg-bodySecundary" />
   <LoadingOverlay v-if="isLoading" />
-  <div class="bg-bodySecundary" v-if="artist">
-    <Container class="flex gap-5 pt-24 pb-10 flex-col items-center md:flex-row">
-      <Avatar :src="artist.image" :size="48" />
-      <div class="flex flex-col justify-end">
-        <span class="font-medium text-textGrey text-center md:text-left">
-          {{
-            t('artist.followers', {
-              count: formatFollowers(artist.followers)
-            })
-          }}</span
-        >
-        <h1 class="text-4xl font-black text-center md:text-6xl">
-          {{ artist.name }}
-        </h1>
-      </div>
-    </Container>
-  </div>
+  <HeroProfile
+    v-if="artist"
+    :name="artist.name"
+    :followers="artist.followers"
+    :image="artist.image"
+  />
   <Container>
     <div v-if="artist">
       <ChipGroup class="mt-5">
-        <Chip v-for="(genre, index) in artist.genres" :key="index">{{ genre }}</Chip>
+        <Link
+          v-for="(genre, index) in artist.genres"
+          :key="index"
+          :to="{ name: 'Genre', params: { tag: genre } }"
+        >
+          <Chip>{{ genre }}</Chip>
+        </Link>
       </ChipGroup>
     </div>
   </Container>
@@ -32,13 +26,14 @@ import { onMounted, Ref, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import api from '~/api';
 
-import Header from '~/components/layout/Header.vue';
 import Container from '~/components/layout/Container.vue';
 import LoadingOverlay from '~/components/base/LoadingOverlay.vue';
-import Avatar from '~/components/base/Avatar.vue';
-import { useI18n } from 'vue-i18n';
+import Link from '~/components/base/Link.vue';
+import HeroProfile from '~/components/base/HeroProfile.vue';
 import ChipGroup from '~/components/base/Chip/ChipGroup.vue';
 import Chip from '~/components/base/Chip/Chip.vue';
+
+import { useI18n } from 'vue-i18n';
 import { BacktrackArtist } from '~/types/backtrack';
 
 const { t } = useI18n();
@@ -60,10 +55,6 @@ const getArtistStreamCount = async (id: number): Promise<number> => {
 const getArtistStreamDuration = async (id: number): Promise<number> => {
   const res = await api.get(`/streams/artist/${id}/duration`);
   return res.data.data;
-};
-
-const formatFollowers = (followers: number): string => {
-  return followers.toLocaleString('en-US');
 };
 
 onMounted(async () => {
