3aa911bd597d53868e01d7d2264bdd6729e6ecad
Martijn-Faber <martijn@fabtronics.nl>
2022-01-01 14:41:10 +0100

    fix(Link): give the link a min width of the max content

--- src/components/base/Link.vue
+++ src/components/base/Link.vue
@@ -1,6 +1,6 @@
 <template>
-  <a v-if="typeof to == 'string' && to.startsWith('http')" :href="to"><slot /></a>
-  <router-link v-else :to="to"><slot /></router-link>
+  <a v-if="typeof to == 'string' && to.startsWith('http')" :href="to" class="min-w-max"><slot /></a>
+  <router-link v-else :to="to" class="min-w-max"><slot /></router-link>
 </template>
 
 <script lang="ts" setup>
