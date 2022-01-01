768a81a0caa9a90a4b939c83cdfdbbe31e19f10a
Martijn-Faber <martijn@fabtronics.nl>
2022-01-01 15:17:52 +0100

    feat(Hero): add the `Hero` component

---
+++ src/components/base/Hero.vue
@@ -0,0 +1,13 @@
+<template>
+  <Header class="bg-bodySecundary" />
+  <div class="bg-bodySecundary">
+    <Container class="flex gap-5 pt-24 pb-10 flex-col items-center md:flex-row">
+      <slot />
+    </Container>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Header from '../layout/Header.vue';
+import Container from '../layout/Container.vue';
+</script>
