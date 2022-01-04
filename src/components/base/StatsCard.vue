656f8d828065f3c5b349614e522fda4a91c8749c
Martijn-Faber <martijn@fabtronics.nl>
2022-01-04 23:58:09 +0100

    feat(StatsCard): add a card to show stats in

---
+++ src/components/base/StatsCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <Card class="text-center h-max">
+    <h1 class="text-primary"><slot /></h1>
+    <span class="text-white">{{ label }}</span>
+  </Card>
+</template>
+
+<script lang="ts" setup>
+import Card from '../layout/Card.vue';
+
+defineProps<{
+  label: string;
+}>();
+</script>
