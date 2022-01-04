949d9ad939a7be0e11b3f4d3e26ffbf93328ce79
Martijn-Faber <martijn@fabtronics.nl>
2022-01-04 23:59:01 +0100

    feat(AudioAnalysis): emit the segment on timestamp hover

--- src/components/base/AudioAnalysis/AudioAnalysis.vue
+++ src/components/base/AudioAnalysis/AudioAnalysis.vue
@@ -1,18 +1,19 @@
 <template>
-  <canvas ref="canvas" />
+  <canvas ref="canvas" @mousemove="onMouseMove" />
 </template>
 
 <script lang="ts" setup>
 import dayjs from '~/dayjs';
 import { onMounted, Ref, ref } from 'vue';
-import { BacktrackAudioAnalysis, Point } from '~/types';
+import { BacktrackAudioAnalysis, BacktrackAudioAnalysisSegment, Point } from '~/types';
 
 const props = defineProps<{
   analysis: BacktrackAudioAnalysis;
 }>();
 
-const canvas: Ref<HTMLCanvasElement | undefined> = ref();
+const emit = defineEmits(['segment']);
 
+const canvas: Ref<HTMLCanvasElement | undefined> = ref();
 const segments = props.analysis.segments;
 
 onMounted(() => {
@@ -21,6 +22,9 @@ onMounted(() => {
   if (canvas.value && ctx) {
     const width = (canvas.value.width = 400);
     const height = (canvas.value.height = 400);
+    canvas.value.style.width = `${width}px`;
+    canvas.value.style.height = `${height}px`;
+
     const centrum: Point = { x: width / 2, y: height / 2 };
 
     let maxLoudness = -Infinity;
@@ -48,14 +52,16 @@ onMounted(() => {
       const radius =
         ((segment.loudness_max - minLoudness) / (maxLoudness - minLoudness)) * (width / 2) - 50;
 
-      const x = centrum.x + radius * Math.cos(angle);
-      const y = centrum.y + -radius * Math.sin(angle);
+      const segmentPos: Point = {
+        x: centrum.x + radius * Math.cos(angle),
+        y: centrum.y + -radius * Math.sin(angle)
+      };
 
       if (isFirst) {
         isFirst = false;
-        ctx.moveTo(x, y);
+        ctx.moveTo(segmentPos.x, segmentPos.y);
       } else {
-        ctx.lineTo(x, y);
+        ctx.lineTo(segmentPos.x, segmentPos.y);
         ctx.stroke();
       }
     }
@@ -66,30 +72,98 @@ onMounted(() => {
   }
 });
 
+const timestamps: Ref<
+  { point: Point; duration: number; formattedDuration: string; index: number }[]
+> = ref([]);
+const blockSegments: BacktrackAudioAnalysisSegment[] = [];
+
 const drawTimeStamps = (ctx: CanvasRenderingContext2D, totalDuration: number) => {
   if (canvas.value) {
     const centrum: Point = { x: canvas.value.width / 2, y: canvas.value.height / 2 };
 
     const durationPartsCount = 9;
-    const durationPerPart = totalDuration / durationPartsCount;
+    const durationPerPart = (totalDuration + 1) / durationPartsCount;
+
+    for (const segment of segments) {
+      if (segment.start >= blockSegments.length * durationPerPart) {
+        blockSegments.push(segment);
+      }
+    }
+
     const anglePerPart = (Math.PI * 2) / durationPartsCount;
 
-    let currentAngle = -anglePerPart;
-    for (let i = 0; i < durationPartsCount; i++) {
-      const partDuration = durationPerPart * i;
+    let currentAngle = -Math.PI / 2;
+    for (let i = 0; i < blockSegments.length; i++) {
+      const partDuration = blockSegments[i].start;
+      const formattedDuration = dayjs.duration(partDuration, 'seconds').format('mm:ss');
       const radius = canvas.value.width / 2 - 25;
-      const pos: Point = {
+
+      const timeStampPos: Point = {
         x: centrum.x + radius * Math.cos(currentAngle),
         y: centrum.x + radius * Math.sin(currentAngle)
       };
 
+      timestamps.value.push({
+        point: timeStampPos,
+        duration: partDuration,
+        formattedDuration,
+        index: i
+      });
+
       ctx.textAlign = 'center';
       ctx.font = 'bold 16px Open Sans';
       ctx.fillStyle = 'white';
-      ctx.fillText(dayjs.duration(partDuration, 'seconds').format('mm:ss'), pos.x, pos.y);
+      ctx.fillText(formattedDuration, timeStampPos.x, timeStampPos.y);
 
       currentAngle += anglePerPart;
     }
   }
+};
+
+const onMouseMove = (e: MouseEvent) => {
+  const ctx = canvas.value?.getContext('2d');
+
+  if (canvas.value && ctx) {
+    const mousePos = getMousePos(canvas.value, e);
+
+    for (const timestamp of timestamps.value) {
+      const bounding = ctx.measureText(timestamp.formattedDuration);
+      const textHeight = bounding.actualBoundingBoxAscent + bounding.actualBoundingBoxDescent;
+      const textWidth = bounding.width;
+
+      if (
+        mousePos.x > timestamp.point.x - textWidth / 2 &&
+        mousePos.x < timestamp.point.x - textWidth / 2 + textWidth
+      ) {
+        if (
+          mousePos.y > timestamp.point.y - textHeight &&
+          mousePos.y < timestamp.point.y + textHeight
+        ) {
+          canvas.value.style.cursor = 'pointer';
+
+          const segment = blockSegments[timestamp.index];
+          const nextSegment = blockSegments[timestamp.index + 1];
+
+          if (nextSegment) {
+            emit('segment', { current: segment, next: nextSegment });
+          } else {
+            emit('segment', { current: segment, next: segments[segments.length - 1] });
+          }
+
+          return;
+        }
+
+        canvas.value.style.cursor = 'initial';
+      }
+    }
+  }
+};
+
+const getMousePos = (canvas: HTMLCanvasElement, e: MouseEvent): Point => {
+  const rect = canvas.getBoundingClientRect();
+  return {
+    x: e.clientX - rect.left,
+    y: e.clientY - rect.top
+  };
 };
 </script>
