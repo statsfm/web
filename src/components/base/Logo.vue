290879f0c0a9ebf5a536e82bbf435d4afbd6fee8
Martijn-Faber <martijn@fabtronics.nl>
2022-01-06 00:39:45 +0100

    fix(Logo): fix logo fill color

--- src/components/base/Logo.vue
+++ src/components/base/Logo.vue
@@ -1,11 +1,5 @@
 <template>
-  <svg
-    width="512"
-    height="512"
-    viewBox="0 0 512 512"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
+  <svg width="512" height="512" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
     <rect y="150" width="110" height="360" rx="40" />
     <rect x="200" width="110" height="512" rx="40" />
     <rect x="400" width="110" height="220" rx="40" y="292" />
@@ -14,6 +8,6 @@
 
 <style lang="scss" scoped>
 svg rect {
-  fill: var(--color-primary);
+  @apply fill-primary;
 }
 </style>
