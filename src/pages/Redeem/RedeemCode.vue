5028dc3e36addde062f3818ae57ebad61ac3b3b5
Martijn-Faber <martijn@fabtronics.nl>
2022-01-06 00:13:48 +0100

    feat(RedeemCode): show simple confetti if the card is flipped

--- src/pages/Redeem/RedeemCode.vue
+++ src/pages/Redeem/RedeemCode.vue
@@ -4,6 +4,7 @@
     <GiftCard
       :giftCode="giftCode"
       :isFlipped="isFlipped"
+      @flipped="isFlipped = $event"
       @click="onGiftCardNotAuthenticatedClick"
       class="mt-24"
     />
@@ -15,7 +16,7 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, Ref, ref } from 'vue';
+import { onMounted, Ref, ref, watch } from 'vue';
 
 import Header from '~/components/layout/Header.vue';
 import Container from '~/components/layout/Container.vue';
@@ -26,6 +27,7 @@ import { useRoute, useRouter } from 'vue-router';
 import Button from '~/components/base/Button.vue';
 import { useI18n } from 'vue-i18n';
 import { useAuth, useToaster } from '~/hooks';
+import JSConfetti from 'js-confetti';
 
 const { t } = useI18n();
 const toaster = useToaster();
@@ -47,6 +49,13 @@ const getGiftCode = async (code: string) => {
 
   giftCode.value = res.data.item;
 };
+
+watch(isFlipped, (val) => {
+  if (val) {
+    const confetti = new JSConfetti();
+    confetti.addConfetti();
+  }
+});
 
 const redeemGiftCode = async () => {
   const res = await BacktrackApi.post<PostPlusGiftCodeRedeemResponse>(
