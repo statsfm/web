9d141893f402a21baa7f0f3e3b40f1e9f3cf9608
Martijn-Faber <martijn@fabtronics.nl>
2022-01-07 18:48:23 +0100

    fix(Avatar): make the `src` prop optional

--- src/components/base/Avatar.vue
+++ src/components/base/Avatar.vue
@@ -3,17 +3,17 @@
     class="bg-bodySecundary rounded-full bg-cover bg-center aspect-square"
     :class="`w-${size}`"
     :style="{ backgroundImage: `url(${src})` }"
-    href="#"
   ></div>
 </template>
 
 <script lang="ts" setup>
 interface Props {
   size: string;
-  src: string;
+  src?: string;
 }
 
 withDefaults(defineProps<Props>(), {
-  size: '12'
+  size: '12',
+  src: '' // TODO: add a placeholder image
 });
 </script>
