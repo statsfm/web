e490b52b43abaf5364f3f1c448e91a67c3ea68b4
Martijn-Faber <martijn@fabtronics.nl>
2022-01-24 15:18:18 +0100

    improvement(TotalStats): use a for loop and localized strings

--- src/components/base/TotalStats/TotalStats.vue
+++ src/components/base/TotalStats/TotalStats.vue
@@ -1,20 +1,32 @@
 <template>
   <div class="grid grid-cols-2 gap-x-4 gap-y-6" v-if="stats">
-    <TotalStatsItem label="Users" :snapshot="stats.users" hasIndicator />
-    <TotalStatsItem label="Plus users" :snapshot="stats.plusUsers" hasIndicator />
-    <TotalStatsItem label="Streams" :snapshot="stats.streams" hasLiveIndicator hasIndicator />
-
-    <TotalStatsItem label="Tracks" :snapshot="stats.tracks" hasIndicator />
-    <TotalStatsItem label="Artists" :snapshot="stats.artists" hasIndicator />
-    <TotalStatsItem label="Albums" :snapshot="stats.albums" hasIndicator />
+    <TotalStatsItem
+      v-for="[key, item] in Object.entries(stats)"
+      :label="labelMap[key]"
+      :snapshot="item"
+      hasIndicator
+      :hasLiveIndicator="key === 'streams'"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, ref, Ref } from 'vue';
+import { useI18n } from 'vue-i18n';
 import BacktrackApi from '~/api';
 import { TotalSize, GetStatsDatabaseSiteResponse } from '~/types';
 import TotalStatsItem from './TotalStatsItem.vue';
+
+const { t } = useI18n();
+
+const labelMap: Record<string, string> = {
+  users: t('total_stats.users'),
+  plusUsers: t('total_stats.plus_users'),
+  streams: t('total_stats.streams'),
+  tracks: t('total_stats.tracks'),
+  artists: t('total_stats.artists'),
+  albums: t('total_stats.albums')
+};
 
 const stats: Ref<TotalSize | null> = ref(null);
 
