2fbeffc4c69b306bbd48151a7944a3ca53243ed0
Martijn-Faber <martijn@fabtronics.nl>
2022-02-03 10:28:38 +0100

    feat(Image): add the image component

---
+++ src/components/base/Image.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import { useAttrs } from 'vue';
+
+type Variant = 'square' | 'round';
+
+interface Props {
+  src: string;
+  alt?: string;
+  variant: Variant;
+}
+
+const variantMap: Record<Variant, string> = {
+  square: 'rounded-lg',
+  round: 'rounded-full'
+};
+
+withDefaults(defineProps<Props>(), {
+  variant: 'square'
+});
+
+const attrs = useAttrs();
+</script>
+
+<template>
+  <Transition
+    enter-active-class="transition ease-out"
+    enter-from-class="transform opacity-0"
+    enter-to-class="transform opacity-100"
+    leave-active-class="transition ease-in"
+    leave-from-class="transform opacity-100"
+    leave-to-class="transform opacity-0"
+    mode="out-in"
+  >
+    <img
+      :src="src"
+      :key="src"
+      :alt="alt"
+      v-bind="attrs"
+      class="object-cover object-center"
+      :class="variantMap[variant]"
+    />
+  </Transition>
+</template>
