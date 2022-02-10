60f1c02797b65e1b63095270310ed66d0b94c20a
Martijn-Faber <martijn@fabtronics.nl>
2022-02-10 23:25:57 +0100

    feat(Input): add the `Input` component

---
+++ src/components/base/Input/Input.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+import { computed, ref, useAttrs, watch } from 'vue';
+
+interface Props {
+  name?: string;
+  description?: string;
+  label?: string;
+  prefix?: string;
+}
+
+const props = defineProps<Props>();
+
+interface Emits {
+  (event: 'input', value: string): void;
+}
+
+const emit = defineEmits<Emits>();
+
+const attrs = useAttrs();
+const value = ref(attrs.value ?? '');
+
+const handleInput = (e: Event) => {
+  emit('input', (e.target as HTMLInputElement).value);
+};
+
+const ariaValue = computed(() => `${props.prefix}${value.value}`);
+</script>
+
+<template>
+  <label v-if="label" :for="name" class="text-lg font-bold text-neutral-400">{{ label }}</label>
+  <div
+    class="py-2 px-4 w-full rounded-lg bg-bodySecundary text-white text-lg font-bold placeholder:text-neutral-600 flex items-center"
+    :class="{ 'mt-1': label }"
+  >
+    <span v-if="prefix">{{ prefix }}</span>
+    <input
+      :aria-label="name"
+      :aria-valuetext="ariaValue"
+      :name="name"
+      v-model="value"
+      @input="handleInput"
+      class="w-full bg-transparent focus:outline-none"
+    />
+  </div>
+  <p v-if="description" class="mt-2 text-sm font-bold text-neutral-400">
+    {{ description }}
+  </p>
+</template>
