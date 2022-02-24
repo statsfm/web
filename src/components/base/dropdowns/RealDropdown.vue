72310619c91c599ea01a61e02154fcb2303616c6
Martijn-Faber <martijn@fabtronics.nl>
2022-02-24 22:59:53 +0100

    fix(RealDropdown): fix the dropdown temporary

--- src/components/base/dropdowns/RealDropdown.vue
+++ src/components/base/dropdowns/RealDropdown.vue
@@ -1,47 +1,47 @@
 <template>
-  <div v-click-away="hide">
+  <div v-click-away="hide" class="relative">
     <div @click="toggle" ref="handler">
       <slot name="button" :active="isActive" />
     </div>
 
     <!-- TODO: add fade out animtion -->
-    <Teleport to="body">
-      <div
-        ref="dropdown"
-        :aria-hidden="isActive"
-        class="absolute right-0 z-20 max-h-96 w-max animate-fadeIn overflow-y-auto shadow-xl"
-        v-show="isActive"
-        @click="hide"
-        @keydown="onKeyDown"
-      >
-        <slot />
-      </div>
-    </Teleport>
+    <!-- <Teleport to="body"> -->
+    <div
+      ref="dropdown"
+      :aria-hidden="isActive"
+      class="absolute right-0 z-20 mt-2 max-h-96 w-max animate-fadeIn overflow-y-auto shadow-xl"
+      v-show="isActive"
+      @click="hide"
+      @keydown="onKeyDown"
+    >
+      <slot />
+    </div>
+    <!-- </Teleport> -->
   </div>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, Ref, ref } from 'vue';
-import { createPopper } from '@popperjs/core';
+// import { createPopper } from '@popperjs/core';
 import { Keys } from '~/types';
 
-type Alignment = 'start' | 'end';
-type BasePlacement = 'top' | 'right' | 'bottom' | 'left';
-type AlignedPlacement = `${BasePlacement}-${Alignment}`;
-type Placement = BasePlacement | AlignedPlacement;
+// type Alignment = 'start' | 'end';
+// type BasePlacement = 'top' | 'right' | 'bottom' | 'left';
+// type AlignedPlacement = `${BasePlacement}-${Alignment}`;
+// type Placement = BasePlacement | AlignedPlacement;
 
-interface Props {
-  placement?: Placement;
-}
+// interface Props {
+//   placement?: Placement;
+// }
 
-const props = withDefaults(defineProps<Props>(), {
-  placement: 'bottom-end'
-});
+// const props = withDefaults(defineProps<Props>(), {
+//   placement: 'bottom-end'
+// });
 
 const isActive = ref(false);
 
-const handler: Ref<HTMLDivElement | undefined> = ref();
-const dropdown: Ref<HTMLDivElement | undefined> = ref();
+// const handler: Ref<HTMLDivElement | undefined> = ref();
+// const dropdown: Ref<HTMLDivElement | undefined> = ref();
 
 const toggle = () => {
   isActive.value = !isActive.value;
@@ -59,19 +59,17 @@ const onKeyDown = (e: KeyboardEvent) => {
   }
 };
 
-onMounted(() => {
-  if (dropdown.value && handler.value) {
-    createPopper(handler.value, dropdown.value, {
-      placement: props.placement,
-      modifiers: [
-        {
-          name: 'offset',
-          options: {
-            offset: [0, 8]
-          }
-        }
-      ]
-    });
-  }
-});
+// onMounted(() => {
+//   createPopper(handler.value!, dropdown.value!, {
+//     placement: props.placement,
+//     modifiers: [
+//       {
+//         name: 'offset',
+//         options: {
+//           offset: [0, 8]
+//         }
+//       }
+//     ]
+//   });
+// });
 </script>
