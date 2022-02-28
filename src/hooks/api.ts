6128cfa9fb0627f3ada390ae42274410718c7cfc
Stijn van der Kolk <stijn@kolkies.dev>
2022-02-28 08:41:54 +0100

    fix: fix accesToken

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -3,7 +3,7 @@ import Api from '@statsfm/statsfm.js';
 export function useApi(): Api {
   const ref = new Api({
     baseUrl: 'https://beta.stats.fm/api/v1',
-    acccessToken: localStorage.getItem('token') ?? undefined
+    accessToken: localStorage.getItem('token') ?? undefined
   });
   return ref;
 }
