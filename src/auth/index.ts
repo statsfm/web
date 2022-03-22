f3496d2922da12a4d793e2aea56b66e503b18487
Sjoerd Bolten <sjoerdabolten@gmail.com>
2022-03-22 21:20:51 +0100

    Update index.ts

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -125,16 +125,16 @@ export default class auth {
 
     if (token?.startsWith('ey')) {
       return true;
-      const { exp, iat } = JSON.parse(Buffer.from(token!.split('.')[1], 'base64').toString()); // falsely marked as deprecated -> https://github.com/microsoft/TypeScript/issues/45566
-      if (exp == undefined || exp == null) {
-        valid = true;
-      } else {
-        valid = Math.floor(new Date().getTime() / 1000) <= exp;
+      // const { exp, iat } = JSON.parse(Buffer.from(token!.split('.')[1], 'base64').toString()); // falsely marked as deprecated -> https://github.com/microsoft/TypeScript/issues/45566
+      // if (exp == undefined || exp == null) {
+      //   valid = true;
+      // } else {
+      //   valid = Math.floor(new Date().getTime() / 1000) <= exp;
 
-        if (valid && maxAge && maxAge > 0) {
-          valid = Date.now() / 1000 - iat < 60;
-        }
-      }
+      //   if (valid && maxAge && maxAge > 0) {
+      //     valid = Date.now() / 1000 - iat < 60;
+      //   }
+      // }
     }
 
     return valid;
