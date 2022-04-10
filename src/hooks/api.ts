1a1b7624d8ddf967497cd4279d17585267c01fce
Sjoerd Bolten <sjoerdabolten@gmail.com>
2022-04-10 06:33:54 +0200

    updated api host

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -2,7 +2,7 @@ import Api from '@statsfm/statsfm.js';
 
 export function useApi(): Api {
   const ref = new Api({
-    baseUrl: 'https://beta.stats.fm/api/v1',
+    baseUrl: 'https://beta-api.stats.fm/api/v1',
     accessToken: localStorage.getItem('token')?.toString() ?? undefined
   });
   return ref;
