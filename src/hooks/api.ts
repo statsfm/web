e44ec69ca892bf1e8f259edf91505239968d17e6
Sjoerd Bolten <sjoerdabolten@gmail.com>
2022-04-11 05:37:39 +0200

    feat: DI api and auth refs

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -1,9 +1,9 @@
 import Api from '@statsfm/statsfm.js';
 
+const ref = new Api({
+  baseUrl: 'https://beta-api.stats.fm/api/v1',
+  accessToken: localStorage.getItem('token')?.toString() ?? undefined
+});
 export function useApi(): Api {
-  const ref = new Api({
-    baseUrl: 'https://beta-api.stats.fm/api/v1',
-    accessToken: localStorage.getItem('token')?.toString() ?? undefined
-  });
   return ref;
 }
