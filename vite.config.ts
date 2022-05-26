f8935de3908bbe4c18067dc90e7ad1b3f50988b8
Martijn-Faber <martijn@fabtronics.nl>
2022-05-26 09:38:53 +0200

    feat(vite): add the jsx plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import jsx from "@vitejs/plugin-vue-jsx";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), jsx()],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
