ff61a8f51432900d11efdefe32d986078d3bbdc9
Martijn-Faber <martijn@fabtronics.nl>
2022-06-01 21:22:53 +0200

    fix(StickyHeader): remove flex and change z-index

--- src/components/base/StickyHeader.vue
+++ src/components/base/StickyHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="sticky top-0 z-10 flex justify-between bg-bodyPrimary pt-10 pb-3">
+  <header class="sticky top-0 z-20 bg-bodyPrimary pt-10 pb-3">
     <slot />
   </header>
 </template>
