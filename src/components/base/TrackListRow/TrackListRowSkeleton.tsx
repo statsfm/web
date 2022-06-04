fc418b679db5d4d18fd97c2343add0769e84e106
Martijn-Faber <martijn@fabtronics.nl>
2022-06-04 12:25:28 +0200

    feat(TrackListRowSkeleton): add divider

--- src/components/base/TrackListRow/TrackListRowSkeleton.tsx
+++ src/components/base/TrackListRow/TrackListRowSkeleton.tsx
@@ -4,16 +4,19 @@ import { FC } from 'vue';
 import { Skeleton } from '../Skeleton';
 
 export const TrackListRowSkeleton: FC = () => (
-  <div class="flex items-center justify-between">
-    <div class="flex items-center gap-3">
-      <Skeleton.Image width="48px" height="48px" />
+  <>
+    <div class="flex items-center justify-between">
+      <div class="flex items-center gap-3">
+        <Skeleton.Image width="48px" height="48px" />
 
-      <div class="flex flex-col gap-2">
-        <Skeleton.Text width="8rem" />
-        <Skeleton.Text width="8rem" />
+        <div class="flex flex-col gap-2">
+          <Skeleton.Text width="8rem" />
+          <Skeleton.Text width="8rem" />
+        </div>
       </div>
-    </div>
 
-    <Skeleton.Text width="4rem" />
-  </div>
+      <Skeleton.Text width="4rem" />
+    </div>
+    <hr class="my-3 border-bodySecundary" />
+  </>
 );
