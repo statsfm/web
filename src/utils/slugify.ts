da3227331c50b94477544c7932220fe597e1dad9
Martijn-Faber <martijn@fabtronics.nl>
2022-06-07 16:54:20 +0200

    feat(slugify): add the slugify function

---
+++ src/utils/slugify.ts
@@ -0,0 +1,10 @@
+export function slugify(text: string) {
+  return text
+    .toString()
+    .normalize('NFKD')
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w\-]+/g, '')
+    .replace(/\-\-+/g, '-');
+}
