fda513ab8de5b17b478c7a4ffbf2e2a7b6a94b11
Martijn-Faber <martijn@fabtronics.nl>
2022-06-08 21:49:58 +0200

    fix(TrackListRow): add `streams` prop and display the count

--- src/components/base/TrackListRow/TrackListRow.tsx
+++ src/components/base/TrackListRow/TrackListRow.tsx
@@ -7,28 +7,37 @@ import { slugify } from '~/utils/slugify';
 import { RouterLink } from 'vue-router';
 import { Image } from '../Image';
 
+// hooks
+import { useI18n } from 'vue-i18n';
+
 interface Props {
   track: statsfm.Track;
-  // streams: statsfm.Stream[];
+  streams: number;
 }
 
-export const TrackListRow: FC<Props> = ({ track }) => (
-  <>
-    <RouterLink
-      to={{ name: 'Track', params: { id: track.id, slug: slugify(track.name) } }}
-      class="flex items-center justify-between"
-    >
-      <div class="flex items-center gap-3">
-        <Image class="w-12" src={track.albums[0].image} />
+export const TrackListRow: FC<Props> = ({ track, streams }) => {
+  const { t } = useI18n();
 
-        <div class="leading-tight">
-          <h4>{track.name}</h4>
-          <p class="m-0">{track.artists[0].name}</p>
+  return (
+    <>
+      <RouterLink
+        to={{ name: 'Track', params: { id: track.id, slug: slugify(track.name) } }}
+        class="flex items-center justify-between"
+      >
+        <div class="flex items-center gap-3">
+          <Image class="w-12" src={track.albums[0].image} />
+
+          <div class="leading-tight">
+            <h4>{track.name}</h4>
+            <p class="m-0">
+              {track.artists[0].name} • {t('times_streamed', { count: streams })}
+            </p>
+          </div>
         </div>
-      </div>
 
-      <p class="text-right">{dayjs.duration(track.durationMs, 'milliseconds').format('m:ss')}</p>
-    </RouterLink>
-    <hr class="my-3 border-bodySecundary" />
-  </>
-);
+        <p class="text-right">{dayjs.duration(track.durationMs, 'milliseconds').format('m:ss')}</p>
+      </RouterLink>
+      <hr class="my-3 border-bodySecundary" />
+    </>
+  );
+};
