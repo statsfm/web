f32ba18d2415abe82a873c1ec72158988f4641cb
Martijn-Faber <martijn@fabtronics.nl>
2022-06-14 16:04:20 +0200

    fix(StickyHeader): center items in y direction

--- src/components/base/StickyHeader.vue
+++ src/components/base/StickyHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="sticky top-0 z-20 flex justify-between bg-bodyPrimary pt-10 pb-3">
+  <header class="sticky top-0 z-20 flex items-center justify-between bg-bodyPrimary pt-10 pb-3">
     <slot />
   </header>
 </template>
