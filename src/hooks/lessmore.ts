591a6d4a3a0dcd1a00b016ec646afed0c4cd9b1c
Martijn-Faber <martijn@fabtronics.nl>
2022-06-15 15:21:58 +0200

    refactor(lessmore): refactor the `useLessMore` hook

--- src/hooks/lessmore.ts
+++ src/hooks/lessmore.ts
@@ -1,23 +1,21 @@
-import { Ref, ref, watch } from 'vue';
+import { ref, Ref, watch } from 'vue';
 
-export const useLessMore = <T>(list: T[], limit: number) => {
-  const showMore = ref(false);
+// hooks
+import { useToggle } from './toggle';
+
+export const useLessMore = <T>(list: Ref<T[]>, limit: number) => {
+  const { state: showMore, on: more, off: less, toggle } = useToggle();
   // https://github.com/vuejs/core/issues/2136
-  const data = ref<T[]>(list.slice(0, limit)) as Ref<T[]>;
+  const data = ref<T[]>(list.value.slice(0, limit)) as Ref<T[]>;
 
-  const less = () => {
-    showMore.value = false;
-  };
+  // TODO: clean up
+  watch(list, (list) => {
+    data.value = showMore.value ? list : list.slice(0, limit);
+  });
 
-  const more = () => {
-    showMore.value = true;
-  };
-
-  const toggle = () => {
-    showMore.value = !showMore.value;
-  };
-
-  watch(showMore, (state) => (data.value = state ? list : list.slice(0, limit)));
+  watch(showMore, (state) => {
+    data.value = state ? list.value : list.value.slice(0, limit);
+  });
 
   return {
     showMore,
