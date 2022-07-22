bed569d3d2fadf0484fb7b6fd4cbe50eabab9098
Martijn-Faber <martijn@fabtronics.nl>
2022-07-22 20:10:55 +0200

    improvement(Container): add option to pass an `as` prop

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,15 +1,25 @@
 import clsx from 'clsx';
-import type { FC, HTMLAttributes } from 'react';
+import type { HTMLAttributes, PropsWithChildren, ElementType } from 'react';
 
-interface Props extends HTMLAttributes<Element> {}
+interface Props extends HTMLAttributes<HTMLElement> {
+  as?: ElementType;
+}
 
-export const Container: FC<Props> = (props) => (
-  <div
-    className={clsx(
-      'mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8',
-      props.className
-    )}
-  >
-    {props.children}
-  </div>
-);
+export const Container = ({
+  as: Component = 'div',
+  className,
+  children,
+  ...props
+}: PropsWithChildren<Props>) => {
+  return (
+    <Component
+      className={clsx(
+        'mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </Component>
+  );
+};
