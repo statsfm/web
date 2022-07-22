4bf2f656cf765c66acb081a9115aa84aa740bf70
Martijn-Faber <martijn@fabtronics.nl>
2022-07-22 11:26:40 +0200

    fix(Skeleton): add the skeleton component

---
+++ src/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,57 @@
+import clsx from 'clsx';
+import type { HTMLAttributes } from 'react';
+import type { AvatarSize } from '../Avatar';
+import { sizes } from '../Avatar';
+
+type Animation = 'pulse';
+
+interface DefaultProps extends HTMLAttributes<HTMLElement> {
+  animation?: Animation;
+}
+
+const animations: Record<Animation, string> = {
+  pulse: 'animate-pulse',
+};
+
+interface ImageProps extends DefaultProps {
+  width: string;
+  height: string;
+}
+
+export const Image = ({ animation = 'pulse', width, height }: ImageProps) => (
+  <span
+    className={clsx(
+      'block rounded-lg bg-foreground',
+      animation && animations[animation]
+    )}
+    style={{ width, height }}
+  />
+);
+
+interface TextProps extends DefaultProps {
+  width: string;
+}
+
+export const Text = ({ animation = 'pulse', width }: TextProps) => (
+  <span
+    className={clsx(
+      'h-4 rounded-full bg-foreground',
+      animation && animations[animation]
+    )}
+    style={{ width }}
+  />
+);
+
+interface AvatarProps extends DefaultProps {
+  size?: AvatarSize;
+}
+
+export const Avatar = ({ animation = 'pulse', size = 'md' }: AvatarProps) => (
+  <div
+    className={clsx(
+      'aspect-square rounded-full bg-foreground',
+      animation && animations[animation]
+    )}
+    style={{ width: sizes[size] }}
+  />
+);
