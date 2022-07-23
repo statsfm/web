39d47a371c570a004cc987505385ee916f8d76b3
Martijn-Faber <martijn@fabtronics.nl>
2022-07-23 16:49:48 +0200

    feat(_app): add the `AuthProvider`, wrap the content in a `Container` and add the `Navbar` and `Footer` components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
 
+import { AuthProvider } from '@/context/auth';
+
+import { NavBar } from '@/components/Navbar';
+import { Footer } from '@/components/Footer';
+import { Container } from '@/components/Container';
+import type { AppProps } from 'next/app';
+
+// TODO: we'll probably rewrite the auth logic to use a state management store instead of context, but we implemented this temporary for development
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
-
-export default App
+  return (
+    <AuthProvider>
+      <NavBar />
+      <Container as="main">
+        <Component {...pageProps} />
+      </Container>
+      <Footer />
+    </AuthProvider>
+  );
+};
+export default App;
