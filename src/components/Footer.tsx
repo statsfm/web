e67aee972ffea6cd24acdfe24e41434dc8b8a0dd
Martijn-Faber <martijn@fabtronics.nl>
2022-07-23 16:42:33 +0200

    fix(Footer): add the download section with app store badges

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import { Container } from './Container';
-import { Logo } from './Logo';
 
 const links: { label: string; links: { label: string; href: string }[] }[] = [
   {
@@ -55,19 +54,11 @@ const links: { label: string; links: { label: string; href: string }[] }[] = [
 export const Footer = () => {
   return (
     <Container as="footer" className="py-14">
-      <div className="grid grid-cols-4 place-items-start gap-x-4">
-        <div>
-          <Logo />
-          <p className="mt-10 min-w-full pt-5 md:order-1 md:mt-0">
-            All rights reserved. Made with ❤️ in the Netherlands.
-          </p>
-        </div>
-
+      <div className="grid grid-flow-row place-items-start gap-4 md:grid-flow-col">
         {links.map((cat, i) => (
           <div key={i} className="grid place-items-start">
             <h4 className="mb-2 text-text-grey">{cat.label}</h4>
             <ul className="grid gap-2">
-              {/* eslint-disable-next-line @typescript-eslint/no-shadow */}
               {cat.links.map((link, i) => (
                 <li key={i}>
                   <Link href={link.href} className="text-[1rem]">
@@ -78,17 +69,44 @@ export const Footer = () => {
             </ul>
           </div>
         ))}
+
+        <div>
+          <h4 className="text-neutral-400">Download</h4>
+          <div className="mt-4"></div>
+          <a
+            className="h-9"
+            href="https://apps.apple.com/app/spotistats-for-spotify/id1526912392"
+            target="blank"
+          >
+            <img
+              src="../../assets/apple_appstore_badge.png"
+              alt="Apple Appstore"
+              className="h-9"
+            />
+          </a>
+          <div className="mt-2"></div>
+          <a
+            className="h-9"
+            href="https://play.google.com/store/apps/details?id=dev.netlob.spotistats"
+            target="blank"
+          >
+            <img
+              src="../../assets/google_play_badge.png"
+              alt="Google Play"
+              className="h-9"
+            />
+          </a>
+        </div>
       </div>
 
-      <p className="mt-10 min-w-full pt-10 text-center text-neutral-500 md:order-1 md:mt-0">
+      <p className="mt-10 min-w-full pt-10 text-center md:order-1 md:mt-0">
         All copyrighted content (i.e. album artwork) on Stats.fm are owned by
         their respective owners. Data is provided by Spotify. Spotify is a
         trademark of Spotify AB. Stats.fm is in no way affiliated with Spotify.
         Spotify is a trademark of Spotify AB. Stats.fm is not affiliated with
         Spotify.
-      </p>
-      <p className="text-center text-text-grey">
-        © 2022 Stats.fm / Spotistats for Spotify.
+        <br />© 2022 Stats.fm / Spotistats for Spotify. All rights reserved.
+        Made with ❤️ in the Netherlands.
       </p>
     </Container>
   );
