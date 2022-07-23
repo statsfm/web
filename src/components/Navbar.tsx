ef3459b4fef29eb67e6ad974f480b1407adf39fa
Martijn-Faber <martijn@fabtronics.nl>
2022-07-23 16:43:53 +0200

    fix(Navbar): use the `Container` `as` prop instead of nesting the container (`div`) inside the nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,22 +9,20 @@ export const NavBar = () => {
   const { user } = useAuth();
 
   return (
-    <nav>
-      <Container className="flex items-center justify-between py-3">
-        <Link href="/" passHref>
-          <a>
-            <Logo className="h-[1.7rem] w-[1.7rem] cursor-pointer" />
-          </a>
-        </Link>
+    <Container as="nav" className="flex items-center justify-between py-3">
+      <Link href="/" passHref>
+        <a>
+          <Logo className="h-[1.7rem] w-[1.7rem] cursor-pointer" />
+        </a>
+      </Link>
 
-        {user ? (
-          <Avatar name={user.displayName} src={user.image} />
-        ) : (
-          <Link href="/login">
-            <Button>Log in</Button>
-          </Link>
-        )}
-      </Container>
-    </nav>
+      {user ? (
+        <Avatar name={user.displayName} src={user.image} />
+      ) : (
+        <Link href="/login">
+          <Button>Log in</Button>
+        </Link>
+      )}
+    </Container>
   );
 };
