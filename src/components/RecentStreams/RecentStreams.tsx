87676a51bfd36e2e684eb2b83f416bca3ea708f8
Martijn-Faber <martijn@fabtronics.nl>
2022-08-18 14:59:14 +0200

    feat(RecentStreams): add the `RecentStreams` component

---
+++ src/components/RecentStreams/RecentStreams.tsx
@@ -0,0 +1,56 @@
+import type * as statsfm from '@statsfm/statsfm.js';
+import dayjs from 'dayjs';
+import { TrackListRow } from '../TrackListRow';
+
+type Props<T extends statsfm.Stream | statsfm.RecentlyPlayedTrack> =
+  T extends statsfm.Stream
+    ? {
+        streams: T[];
+        track?: statsfm.Track;
+      }
+    : {
+        streams: T[];
+        track?: undefined;
+      };
+
+export const RecentStreams = <
+  T extends statsfm.Stream | statsfm.RecentlyPlayedTrack
+>({
+  streams,
+  track,
+}: Props<T>) => {
+  const pairs: Record<string, T[]> = {};
+
+  streams.forEach((stream) => {
+    const key = dayjs(stream.endTime).format('YYYYMMDD');
+
+    if (!pairs[key]) pairs[key] = [];
+    pairs[key]?.push(stream as T);
+  });
+
+  return (
+    <ul>
+      {Object.entries(pairs)
+        .sort((a, b) => {
+          if (a[0] > b[0]) return -1;
+          if (a[0] < b[0]) return 1;
+          return 0;
+        })
+        .map((streams, i) => (
+          <li key={i}>
+            {/* TODO: make the date sticky */}
+            <p>{dayjs(streams[1][0]!.endTime).format('LL')}</p>
+
+            <ul key={i}>
+              {streams[1].map((stream, i) => (
+                <li key={i}>
+                  {/* @ts-ignore TODO: fix type */}
+                  <TrackListRow {...stream} track={stream.track ?? track} />
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+    </ul>
+  );
+};
