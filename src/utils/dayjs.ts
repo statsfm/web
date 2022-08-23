9b5093da720efbc1d8084dae69013fd2199b12dc
Martijn-Faber <martijn@fabtronics.nl>
2022-08-24 00:23:49 +0200

    fix(dayjs): add the `localizedFormat` plugin

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -1,8 +1,10 @@
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
 
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
+dayjs.extend(localizedFormat);
 
 export default dayjs;
