a47a8d0e158e94866b6c9f6f65b080740679c01f
Sjoerd Bolten <sjoerdabolten@gmail.com>
2022-08-24 23:06:05 +0200

    feat(image): added temp placeholder (blur)

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -8,13 +8,23 @@ interface Props extends NextImageProps {
 
 export const Image = ({ rounded = false, className, ...props }: Props) => {
   return (
-    <NextImage
+    <div
       className={clsx(
-        'bg-foreground before:grid before:h-full before:place-items-center before:p-2 before:text-center',
-        rounded ? 'rounded-full' : 'rounded-lg',
-        className
+        'overflow-hidden',
+        rounded ? 'rounded-full' : 'rounded-lg'
       )}
-      {...props}
-    />
+    >
+      <NextImage
+        className={clsx(
+          'bg-foreground before:grid before:h-full before:place-items-center before:p-2 before:text-center',
+          rounded ? 'rounded-full' : 'rounded-lg',
+          className
+        )}
+        objectFit="cover"
+        placeholder="blur"
+        blurDataURL="https://media.discordapp.net/attachments/756495774975655987/1012102493716611132/Rectangle_44.png"
+        {...props}
+      />
+    </div>
   );
 };
