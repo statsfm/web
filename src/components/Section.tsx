131862d7588faf43c8ac6e93cdc64170e543f074
Martijn-Faber <martijn@fabtronics.nl>
2022-09-05 11:24:32 +0200

    feat(Section): add arrow keys event listeners for carousel navigation

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,4 +1,12 @@
-import type { HTMLAttributes, PropsWithChildren } from 'react';
+import { Keys } from '@/types/keyboard';
+import type {
+  HTMLAttributes,
+  KeyboardEventHandler,
+  PropsWithChildren,
+} from 'react';
+import { useContext } from 'react';
+import { ActionType } from './Carousel';
+import { CarouselContext } from './Carousel/context';
 
 interface Props extends HTMLAttributes<HTMLElement> {
   title: string;
@@ -13,8 +21,28 @@ export const Section = ({
   children,
   ...props
 }: PropsWithChildren<Props>) => {
+  const context = useContext(CarouselContext);
+
+  // TODO: move event handler
+  const handleKeyDown: KeyboardEventHandler = (e) => {
+    if (context) {
+      const [state, dispatch] = context;
+
+      switch (e.key) {
+        case Keys.ArrowLeft:
+          if (!state.isPreviousDisabled)
+            dispatch({ type: ActionType.Previous });
+          break;
+        case Keys.ArrowRight:
+          if (!state.isNextDisabled) dispatch({ type: ActionType.Next });
+          break;
+        default:
+      }
+    }
+  };
+
   return (
-    <section>
+    <section onKeyDown={handleKeyDown}>
       <header className="sticky top-0 z-20 flex items-center justify-between bg-background pt-10 pb-3">
         <div>
           <h2>{title}</h2>
