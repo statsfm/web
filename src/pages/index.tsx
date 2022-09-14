765c4426933c63193972f6421aafa3f6a305ab1b
wouter <wouter@debruijn.dev>
2022-09-14 22:21:32 +0200

    feat(home): title on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { TotalStats } from '@/components/Home/TotalStats';
 import { StoreBadge } from '@/components/StoreBadges';
 import clsx from 'clsx';
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import Link from 'next/link';
 import type { FC, PropsWithChildren } from 'react';
 import type { IconType } from 'react-icons';
@@ -57,6 +58,9 @@ const PhoneAppDisplay: FC = () => (
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Stats.fm</title>
+      </Head>
       <Container className="flex flex-col justify-between gap-5 lg:flex-row">
         <div className="my-12 w-full pt-10 lg:my-28 lg:w-5/12">
           <h1 className="text-5xl leading-none">
