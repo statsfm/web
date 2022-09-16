11bb2b7ff33f54dc65c91b544351d0dd3b5bf5bc
wouter <wouter@debruijn.dev>
2022-09-16 12:00:44 +0200

    fix(NavBar): fixed link clickability and login button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,6 @@ import { useAuth } from '@/hooks';
 import { MdAccountCircle, MdExitToApp, MdManageAccounts } from 'react-icons/md';
 import { Logo } from './Logo';
 import { Avatar } from './Avatar/Avatar';
-import { Button } from './Button';
 import { Menu } from './Menu';
 import { Container } from './Container';
 
@@ -39,11 +38,19 @@ export const NavBar = () => {
                   <p className="m-0">{user.email}</p>
                 </div>
               </Menu.Item>
-              <Menu.Item icon={<MdAccountCircle />}>
-                <Link href={`/${user.customId ?? user.id}`}>My page</Link>
+              <Menu.Item>
+                <Link href={`/${user.customId ?? user.id}`}>
+                  <a className="flex h-full w-full flex-row gap-2">
+                    <MdAccountCircle className="text-white" /> My page
+                  </a>
+                </Link>
               </Menu.Item>
-              <Menu.Item disabled icon={<MdManageAccounts />}>
-                My account
+              <Menu.Item>
+                <Link href="/account">
+                  <a className="flex h-full w-full flex-row gap-2">
+                    <MdManageAccounts className="text-white" /> My account
+                  </a>
+                </Link>
               </Menu.Item>
               <Menu.Item icon={<MdExitToApp />} onClick={handleLogOutClick}>
                 Log out
@@ -52,8 +59,9 @@ export const NavBar = () => {
           </Menu>
         ) : (
           <Link href="/login">
-            {/* TODO: same height as avatar to prevent jank */}
-            <Button style={{ height: 64 }}>Log in</Button>
+            <a className="my-2 flex h-12 items-center justify-center whitespace-nowrap rounded-2xl bg-primary/10 py-2 px-5 text-base font-bold text-primary shadow-sm transition-colors hover:bg-primary/20 active:bg-primary/5">
+              Log in
+            </a>
           </Link>
         )}
       </Container>
