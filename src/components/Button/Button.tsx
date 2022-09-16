38401ba53dc08f299405328cddf889c4f596c32c
wouter <wouter@debruijn.dev>
2022-09-16 18:35:19 +0200

    chore(Button): added disabled state

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,20 @@
 import type { HTMLAttributes, PropsWithChildren } from 'react';
 import clsx from 'clsx';
 
-interface Props extends HTMLAttributes<HTMLButtonElement> {}
+interface Props extends HTMLAttributes<HTMLButtonElement> {
+  disabled?: boolean;
+}
 
-const Button = ({ children, ...props }: PropsWithChildren<Props>) => {
+const Button = ({ disabled, children, ...props }: PropsWithChildren<Props>) => {
   return (
     <button
+      {...props}
       onClick={props.onClick}
       className={clsx(
-        'inline-flex items-center justify-center whitespace-nowrap rounded-2xl bg-primary/10 py-3 px-5 text-base font-bold text-primary shadow-sm transition-colors hover:bg-primary/20 active:bg-primary/5',
-        props.className
+        props.className,
+        disabled ? 'bg-primary/20' : 'hover:bg-primary/20 active:bg-primary/5',
+        'inline-flex items-center justify-center whitespace-nowrap rounded-2xl bg-primary/10 py-3 px-5 text-base font-bold text-primary shadow-sm transition-colors'
       )}
-      {...props}
     >
       {children}
     </button>
