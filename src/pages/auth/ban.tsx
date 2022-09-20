336b3c626ac160d33eaa246700d532e8baf17583
Martijn-Faber <martijn@fabtronics.nl>
2022-09-20 19:58:41 +0200

    feat(ban): show `reason` message

--- src/pages/auth/ban.tsx
+++ src/pages/auth/ban.tsx
@@ -1,27 +1,29 @@
 import type { NextPage } from 'next';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 const Ban: NextPage = () => {
   const router = useRouter();
 
+  const [reason, setReason] = useState('');
+
   useEffect(() => {
     // TODO: look why router.params doesn't work
     const params = new URLSearchParams(window.location.search);
 
-    if (params.has('reason')) {
-      // const reason = params.get('reason');
-    } else {
+    if (!params.has('reason')) {
       // TODO: show toaster error
       router.push('/');
     }
+
+    setReason(params.get('reason')!);
   }, []);
 
   // TODO: show some loading ui
   return (
     <>
-      <div>reason</div>
+      <div>reason: {reason}</div>
     </>
   );
 };
