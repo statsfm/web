9657e5563ea854bb4b81402c3d5497991108494c
wouter <wouter@debruijn.dev>
2022-09-20 17:00:47 +0200

    chore(ChipGroup): removed scroll handler

--- src/components/Chip/ChipGroup.tsx
+++ src/components/Chip/ChipGroup.tsx
@@ -1,28 +1,12 @@
 import clsx from 'clsx';
-import type {
-  HTMLAttributes,
-  PropsWithChildren,
-  WheelEventHandler,
-} from 'react';
-import { useRef } from 'react';
+import type { HTMLAttributes, PropsWithChildren } from 'react';
 
 interface Props extends HTMLAttributes<HTMLUListElement> {}
 
 const ChipGroup = ({ children, ...props }: PropsWithChildren<Props>) => {
-  const ref = useRef<HTMLUListElement>(null);
-
-  const handleWheelScroll: WheelEventHandler = (e) => {
-    if (ref.current) ref.current.scrollLeft += e.deltaY || e.deltaX; // deltaX for trackpads
-  };
-
   return (
     <ul
-      className={clsx(
-        'flex flex-nowrap gap-2 overflow-x-auto',
-        props.className
-      )}
-      onWheel={handleWheelScroll}
-      ref={ref}
+      className={clsx('flex gap-2 overflow-y-auto', props.className)}
       {...props}
     >
       {children}
