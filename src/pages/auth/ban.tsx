8a3414b31527aa3d0838eec4b17fc72934f29bf6
Sjoerd Bolten <sjoerdabolten@gmail.com>
2022-09-20 20:23:59 +0200

    feat(ban): added ban content

--- src/pages/auth/ban.tsx
+++ src/pages/auth/ban.tsx
@@ -2,11 +2,14 @@ import type { NextPage } from 'next';
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import { Container } from '@/components/Container';
+import { ChevronLink } from '@/components/ChevronLink';
 
 const Ban: NextPage = () => {
   const router = useRouter();
 
   const [reason, setReason] = useState('');
+  const [bannedAt, setBannedAt] = useState(new Date());
 
   useEffect(() => {
     // TODO: look why router.params doesn't work
@@ -18,12 +21,34 @@ const Ban: NextPage = () => {
     }
 
     setReason(params.get('reason')!);
+    setBannedAt(new Date(params.get('bannedAt')!));
   }, []);
 
-  // TODO: show some loading ui
   return (
     <>
-      <div>reason: {reason}</div>
+      <div className="bg-bodySecundary pt-20">
+        <Container>
+          <section className="flex flex-col items-center gap-5 pt-24 pb-10 md:flex-row">
+            <div className="flex flex-col justify-end">
+              <h1 className="text-center text-5xl font-extrabold capitalize md:text-left">
+                Banned
+              </h1>
+            </div>
+          </section>
+        </Container>
+      </div>
+
+      <Container className="mt-4">
+        <h3>Banned at</h3>
+        <span className="text-base">{bannedAt.toLocaleString()}</span>
+        <h3 className="mt-3">Reason</h3>
+        <span className="text-base">{reason}</span>
+        <br />
+        <br />
+        <ChevronLink href="https://support.stats.fm/docs/banned">
+          More info
+        </ChevronLink>
+      </Container>
     </>
   );
 };
