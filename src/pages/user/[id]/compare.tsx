5dbcb8192ba7b61884fb28545f8d0fd3e65c0be4
wouter <wouter@debruijn.dev>
2022-11-04 15:43:59 +0100

    chore(compare): fixed typo

--- src/pages/user/[id]/compare.tsx
+++ src/pages/user/[id]/compare.tsx
@@ -6,16 +6,15 @@ import type { NextPage } from 'next';
 const ComparePage: NextPage = () => {
   return (
     <Container className="flex min-h-screen items-center">
-      <Title>Comming soon</Title>
+      <Title>Coming soon</Title>
       <div className="flex w-full flex-col justify-center">
         <h1 className="mb-0 text-center text-4xl font-extrabold sm:text-[5rem]">
-          Comming soon,
+          Coming soon,
         </h1>
         <p className="mt-4 text-center text-lg sm:-mt-2">
-          We are hard at work to bring this functionality to the website,
-        </p>
-        <p className="-mt-1 text-center text-lg sm:-mt-2 ">
-          for now you can use the app.
+          We are hard at work to bring this functionality to the website.{' '}
+          <br className="mb-1 sm:mb-0" />
+          For now, you can use the app.
         </p>
         <div className=" mx-auto mt-8 flex w-fit flex-col items-center gap-2">
           <StoreBadge store="apple" />
