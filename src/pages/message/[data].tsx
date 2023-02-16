0f05e346a1010e7cebbdef91d50bb91b8f64669b
Stijn van der Kolk <stijn@kolkies.dev>
2023-02-16 23:55:19 +0100

    feat: upgrade message page

--- src/pages/message/[data].tsx
+++ src/pages/message/[data].tsx
@@ -7,11 +7,14 @@ type BaseProps = {
   code: number;
   title: string;
   message: string;
-  action?: string | null;
-  url?: string | null;
+  buttons?: {
+    action: string;
+    url: string;
+  }[];
+  hideCode?: boolean;
 };
 
-type Props = BaseProps | Required<BaseProps>;
+type Props = Required<BaseProps>;
 
 const unparseData = (unparsedData: string): Props => {
   const vars = unparsedData.split(';');
@@ -20,6 +23,8 @@ const unparseData = (unparsedData: string): Props => {
     .map<{ [K in keyof Props]: Props[K] }>((v) => {
       const [key, value] = v.split('=');
       if (key === 'code' && value) return { code: parseInt(value, 10) };
+      if (key === 'hideCode' && value) return { hideCode: value === 'true' };
+      if (key === 'buttons' && value) return { buttons: JSON.parse(value) };
       if (key && value) return { [key]: decodeURIComponent(value) };
       return null as any;
     })
@@ -27,15 +32,18 @@ const unparseData = (unparsedData: string): Props => {
 
   const data = mappedVars.reduce(
     (acc, curr) => ({ ...acc, ...curr }),
-    {} as Props
+    {} as BaseProps
   );
+
   // Insert default vars
   return {
     code: data.code ?? 400,
     title: data.title ?? 'Error',
     message: data.message ?? 'Something went wrong',
-    action: data.action ?? null,
-    url: data.url ?? null,
+    buttons: (data.buttons ?? []).filter(
+      (button) => button.action && button.url
+    ),
+    hideCode: data.hideCode ?? false,
   };
 };
 
@@ -60,17 +68,21 @@ const MessagePage: NextPage<Props> = (data) => {
       <Title>{data.title}</Title>
       <div className="flex w-full flex-col justify-center">
         <h2 className="mb-0 text-center text-3xl font-bold lg:text-[3rem]">
-          <span className="mr-2 text-gray-500">{data.code}</span>
+          {!data.hideCode ? (
+            <span className="mr-2 text-gray-500">{data.code}</span>
+          ) : null}
           {data.title}
         </h2>
         <p className="-mt-2 text-center text-xl">{data.message}</p>
-        {data.action && data.url && (
-          <Link legacyBehavior href={data.url}>
-            <a className="mx-auto mt-8 w-fit rounded-xl bg-primaryLighter px-5 py-2 font-bold text-primary">
-              {data.action}
-            </a>
-          </Link>
-        )}
+        <div className="mt-5 flex w-full flex-row justify-center space-x-4">
+          {data.buttons.map((b) => (
+            <Link legacyBehavior href={b.url} key={`${b.action}-${b.url}`}>
+              <a className="flex w-fit rounded-xl bg-primaryLighter px-5 py-2 font-bold text-primary">
+                {b.action}
+              </a>
+            </Link>
+          ))}
+        </div>
       </div>
     </Container>
   );
