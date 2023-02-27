93cca7b415229f4e6d1761153796e3dc7b34ef74
Sjoerd Bolten <sjoerdabolten@gmail.com>
2023-02-27 23:06:22 +0100

    feat(plus): added clickfunnel redirects

---
+++ src/pages/plus/offer/callback.tsx
@@ -0,0 +1,39 @@
+import { Container } from '@/components/Container';
+import type { GetServerSideProps, NextPage } from 'next';
+
+type Props = {};
+
+export const getServerSideProps: GetServerSideProps<Props> = async ({
+  query,
+}) => {
+  const { code, state } = query;
+
+  if (typeof code !== 'string' || !code.startsWith('ey')) {
+    return {
+      redirect: {
+        permanent: true,
+        destination: '/404',
+      },
+    };
+  }
+
+  // @ts-expect-error error because of atob
+  const { userId } = JSON.parse(atob((code as string).split('.')[1]) as string);
+
+  return {
+    redirect: {
+      permanent: true,
+      destination: `${state}?cf_uivd=${userId + userId}`,
+    },
+  };
+};
+
+const SuccessPage: NextPage<Props> = () => {
+  return (
+    <Container className="min-h-screen pt-20">
+      <div className="mt-20 grid place-items-center text-center"></div>
+    </Container>
+  );
+};
+
+export default SuccessPage;
---
+++ src/pages/plus/offer/index.tsx
@@ -0,0 +1,38 @@
+import { Container } from '@/components/Container';
+import type { GetServerSideProps, NextPage } from 'next';
+
+type Props = {};
+
+export const getServerSideProps: GetServerSideProps<Props> = async ({
+  query,
+}) => {
+  const { url } = query;
+
+  if (typeof url !== 'string' || !url.startsWith('https://plus.stats.fm')) {
+    return {
+      redirect: {
+        permanent: true,
+        destination: '/404',
+      },
+    };
+  }
+
+  const authUrl = `https://api.stats.fm/api/v1/auth/redirect/spotify?scope=user-read-email&redirect_uri=https://stats.fm/plus/offer/callback&state=${url}`;
+
+  return {
+    redirect: {
+      permanent: true,
+      destination: authUrl,
+    },
+  };
+};
+
+const SuccessPage: NextPage<Props> = () => {
+  return (
+    <Container className="min-h-screen pt-20">
+      <div className="mt-20 grid place-items-center text-center"></div>
+    </Container>
+  );
+};
+
+export default SuccessPage;
