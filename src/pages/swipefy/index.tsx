44c189bcaf1d2aa535b2d47fc22a3aa64cab47f0
Sjoerd Bolten <sjoerdabolten@gmail.com>
2023-03-11 02:32:14 +0100

    feat(swipefy): OG image

--- src/pages/swipefy/index.tsx
+++ src/pages/swipefy/index.tsx
@@ -1,6 +1,7 @@
 import { Container } from '@/components/Container';
 import { Title } from '@/components/Title';
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const SwipefyLanding: NextPage = () => {
   return (
@@ -16,8 +17,24 @@ const SwipefyLanding: NextPage = () => {
       </div>
       <Container className="flex min-h-screen items-center">
         <Title>Swipefy</Title>
+        <Head>
+          <meta
+            property="og:image"
+            content={
+              'https://media.discordapp.net/attachments/1083785883519565977/1083787881748893706/Frame_20.png'
+            }
+          />
+          <meta property="og:image:width" content="1024" />
+          <meta property="og:image:height" content="500" />
+          <meta property="og:title" content={`Swipefy by stats.fm`} />
+          <meta
+            property="og:description"
+            content={`The all new way to discover music`}
+          />
+          <meta property="twitter:card" content="summary" />
+        </Head>
         <div className="z-[1000] flex w-full flex-col justify-center pt-[30vh]">
-          <h1 className="mb-0 text-center text-[5rem] font-extrabold italic text-swipefy md:text-[10rem] lg:text-[12rem] xl:text-[15rem]">
+          <h1 className="mb-0 text-center text-[5rem] font-extrabold italic text-swipefy md:text-[10rem] lg:text-[12rem] xl:text-[15rem] ">
             Swipefy
           </h1>
           <p className="-mt-2 text-center text-[2rem] font-bold text-stone-500 md:-mt-10 md:text-[3rem] lg:text-[4rem] xl:text-[5rem]">
