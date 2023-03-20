575e648b313dddaa1c659542cb91e1db324ab7ba
Sjoerd Bolten <sjoerdabolten@gmail.com>
2023-03-20 21:23:53 +0100

    Update callback.tsx

--- src/pages/plus/offer/callback.tsx
+++ src/pages/plus/offer/callback.tsx
@@ -28,7 +28,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({
       permanent: true,
       destination: `${redirectUrl}${
         redirectUrl?.indexOf('?') === -1 ? '?' : '&'
-      }cf_uivd=${encodeURIComponent(me.id)}&email=${
+      }cf_uivd=${encodeURIComponent(me.id)}&emailaddress=${
         encodeURIComponent(me.email) ?? ''
       }&image=${encodeURIComponent(me.image ?? '')}&name=${encodeURIComponent(
         me.displayName
