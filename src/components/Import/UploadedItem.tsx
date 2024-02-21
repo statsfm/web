f24243d976883b64a5f670897f2b66a0c4e25db2
Stijn van der Kolk <stijn@kolkies.dev>
2024-02-21 18:02:59 +0100

    fix(import): only show aprox streams for spotify

--- src/components/Import/UploadedItem.tsx
+++ src/components/Import/UploadedItem.tsx
@@ -6,6 +6,7 @@ import {
   UPLOADED_FILE_STATUS_COLORS,
   UploadedFilesStatus,
 } from '@/utils/imports';
+import { Platform } from '@/utils/statsfm';
 import { Button } from '../Button';
 
 export const UploadedItem: FC<
@@ -26,15 +27,14 @@ export const UploadedItem: FC<
           {UPLOADED_FILE_STATUS[data.status]}
         </div>
       </div>
-      <div className="mt-1 flex items-center gap-x-2 text-sm leading-5 text-gray-500">
-        <p className="truncate">
-          aprox{' '}
-          {data.status === UploadedFilesStatus.Error
-            ? '0'
-            : data.data.length.toLocaleString()}{' '}
-          streams
-        </p>
-      </div>
+      {data.service === Platform.SPOTIFY &&
+        data.status !== UploadedFilesStatus.Error && (
+          <div className="mt-1 flex items-center gap-x-2 text-sm leading-5 text-gray-500">
+            <p className="truncate">
+              aprox {data.data.length.toLocaleString()} streams
+            </p>
+          </div>
+        )}
     </div>
     <div className="flex flex-none gap-x-4">
       <Button
