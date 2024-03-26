c9055dd325c9cada56c36eca690a1682ceb0aba4
Stijn van der Kolk <stijn@kolkies.dev>
2024-03-26 23:06:01 +0100

    fix: use ??

--- src/utils/images/index.ts
+++ src/utils/images/index.ts
@@ -220,7 +220,7 @@ function parseCacheControl(
 export function getMaxAge(str: string | null | undefined): number {
   const map = parseCacheControl(str);
   if (map) {
-    let age = map.get('s-maxage') || map.get('max-age') || '';
+    let age = map.get('s-maxage') ?? map.get('max-age') ?? '';
     if (age.startsWith('"') && age.endsWith('"')) {
       age = age.slice(1, -1);
     }
