58bcc1a3e1ad46912725b08bb0f23fe15fd15200
Martijn-Faber <martijn@fabtronics.nl>
2024-04-10 17:21:50 +0200

    fix(Coupon): fix bug where modal outside click would trigger open state and add cursor pointer

--- src/components/Gift/Coupon.tsx
+++ src/components/Gift/Coupon.tsx
@@ -9,45 +9,51 @@ export const Coupon: FC<{ giftcode: GiftCode }> = ({ giftcode }) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div
-      className="max-h-max max-w-max rounded-2xl bg-foreground p-5"
-      onClick={() => {
-        setOpen(true);
-      }}
-    >
-      <span
-        className="w-max cursor-pointer select-none rounded-xl bg-primary/10 px-4 py-1 font-bold text-primary"
-        onClick={() => navigator.clipboard.writeText(giftcode.code)}
+    <>
+      <div
+        className="max-h-max max-w-max rounded-2xl bg-foreground p-5 cursor-pointer"
+        onClick={() => setOpen(true)}
       >
-        {giftcode.code}
-      </span>
-      {giftcode.claimedAt ? (
-        <p className="mt-3 text-center">
-          Claimed {dayjs(giftcode.claimedAt).fromNow()} by{' '}
-          <Link
-            legacyBehavior
-            href={`/user/${
-              giftcode.claimedBy ? giftcode.claimedBy.id : giftcode.claimedById
-            }`}
-          >
-            <a className="font-bold text-white hover:opacity-70">
-              {giftcode.claimedBy
-                ? giftcode.claimedBy.displayName
-                : giftcode.claimedById}
-            </a>
-          </Link>
-        </p>
-      ) : (
-        <p className="mb-0 mt-3 text-center">
-          Purchased {dayjs(giftcode.purchasedAt).fromNow()}
-        </p>
-      )}
+        <span
+          className="w-max cursor-pointer select-none rounded-xl bg-primary/10 px-4 py-1 font-bold text-primary"
+          onClick={() => navigator.clipboard.writeText(giftcode.code)}
+        >
+          {giftcode.code}
+        </span>
+        {giftcode.claimedAt ? (
+          <p className="mt-3 text-center">
+            Claimed {dayjs(giftcode.claimedAt).fromNow()} by{' '}
+            <Link
+              legacyBehavior
+              href={`/user/${
+                giftcode.claimedBy
+                  ? giftcode.claimedBy.id
+                  : giftcode.claimedById
+              }`}
+            >
+              <a className="font-bold text-white hover:opacity-70">
+                {giftcode.claimedBy
+                  ? giftcode.claimedBy.displayName
+                  : giftcode.claimedById}
+              </a>
+            </Link>
+          </p>
+        ) : (
+          <p className="mb-0 mt-3 text-center">
+            Purchased {dayjs(giftcode.purchasedAt).fromNow()}
+          </p>
+        )}
+      </div>
+
       {/* TODO: move this modal to a more central place bc this is not optimal for performance */}
       <CouponModal
         open={open}
-        onClose={() => setOpen(false)}
+        onClose={() => {
+          setOpen(false);
+          console.log('close: ', open);
+        }}
         giftCode={giftcode}
       />
-    </div>
+    </>
   );
 };
