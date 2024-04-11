bd076ee8ad4c420c208229154e1ea9fe93331302
Xavier B <hello@ncbr.wtf>
2024-04-11 00:55:26 -0400

    refactor(friendsbutton): clean up & keep style consistent when banned

--- src/components/User/FriendsButton.tsx
+++ src/components/User/FriendsButton.tsx
@@ -44,55 +44,6 @@ export const FriendsButton: FC<{
     // Do nothing
   };
 
-  if (friendUser.userBan) {
-    switch (friendStatus) {
-      case FriendStatus.FRIENDS:
-        return (
-          <FriendsButtonFrame red handler={handleRemove}>
-            <span className="flex items-center">
-              <span className="mr-2">
-                <MdInfo />
-              </span>
-              Remove friend
-            </span>
-          </FriendsButtonFrame>
-        );
-      case FriendStatus.REQUEST_INCOMING:
-        return (
-          <FriendsButtonFrame handler={handleDeny}>
-            <span className="flex items-center">
-              <span className="mr-2">
-                <MdInfo />
-              </span>
-              Decline friend request
-            </span>
-          </FriendsButtonFrame>
-        );
-      case FriendStatus.REQUEST_OUTGOING:
-        return (
-          <FriendsButtonFrame red handler={handleCancel}>
-            <span className="flex items-center">
-              <span className="mr-2">
-                <MdInfo />
-              </span>
-              Cancel friend request
-            </span>
-          </FriendsButtonFrame>
-        );
-      default:
-        return (
-          <FriendsButtonFrame handler={noop}>
-            <span className="flex items-center">
-              <span className="mr-2">
-                <MdInfo />
-              </span>
-              This user has been banned, you cannot send a friend request
-            </span>
-          </FriendsButtonFrame>
-        );
-    }
-  }
-
   switch (friendStatus) {
     case FriendStatus.FRIENDS:
       return (
@@ -121,6 +72,18 @@ export const FriendsButton: FC<{
         </FriendsButtonFrame>
       );
     default:
+      if (friendUser.userBan?.active) {
+        return (
+          <FriendsButtonFrame handler={noop}>
+            <span className="flex items-center">
+              <span className="mr-2">
+                <MdInfo />
+              </span>
+              This user has been banned, you cannot send a friend request
+            </span>
+          </FriendsButtonFrame>
+        );
+      }
       return (
         <FriendsButtonFrame handler={handleSend}>
           Send friend request
