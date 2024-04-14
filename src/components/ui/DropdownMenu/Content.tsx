b4246ca8597c1b47421a68ab781baa826b8c399b
Martijn-Faber <martijn@fabtronics.nl>
2024-04-14 16:43:04 +0200

    fix(ui/DropdownMenu): pass custom classnames

--- src/components/ui/DropdownMenu/Content.tsx
+++ src/components/ui/DropdownMenu/Content.tsx
@@ -1,5 +1,6 @@
 import { ElementRef, PropsWithChildren, forwardRef } from 'react';
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu';
+import clsx from 'clsx';
 
 type DropdownMenuContentProps =
   PropsWithChildren<DropdownMenuPrimitive.DropdownMenuContentProps>;
@@ -7,11 +8,14 @@ type DropdownMenuContentProps =
 export const Content = forwardRef<
   ElementRef<typeof DropdownMenuPrimitive.Content>,
   DropdownMenuContentProps
->(({ children, sideOffset = 10, ...props }, forwardedRef) => {
+>(({ children, sideOffset = 10, className, ...props }, forwardedRef) => {
   return (
     <DropdownMenuPrimitive.Content
       ref={forwardedRef}
-      className="min-w-56 rounded-xl bg-foreground py-2 shadow-xl z-50"
+      className={clsx(
+        className,
+        'min-w-56 rounded-xl bg-foreground py-2 shadow-xl z-50',
+      )}
       sideOffset={sideOffset}
       {...props}
     >
