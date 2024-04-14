e0a7f641b1374fbac3374b0706f63e716242c3ec
Martijn-Faber <martijn@fabtronics.nl>
2024-04-14 15:42:10 +0200

    feat(Navbar): replace old `Menu` component with rewritten `DropdownMenu` component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,7 @@
 import Link from 'next/link';
 import { useAuth } from '@/hooks';
 
-import { MdAccountCircle, MdExitToApp, MdManageAccounts } from 'react-icons/md';
-import { Transition } from '@headlessui/react';
+import { MdAccountCircle, MdExitToApp, MdSettings } from 'react-icons/md';
 import { event } from 'nextjs-google-analytics';
 import type { NextRouter } from 'next/router';
 import { useRouter } from 'next/router';
@@ -10,10 +9,10 @@ import Cookies from 'js-cookie';
 import type { UserPrivate } from '@statsfm/statsfm.js';
 import { Logo } from './Logo';
 import { Avatar } from './Avatar/Avatar';
-import { Menu } from './Menu';
 import { Container } from './Container';
 import { Button } from './Button';
 import { CrownIcon } from './Icons';
+import { DropdownMenu } from './ui/DropdownMenu';
 
 const navigation = [
   {
@@ -56,6 +55,7 @@ export const NavBar = () => {
   };
 
   const handleLogOutClick = () => {
+    event('NAV_logout');
     logout();
   };
 
@@ -121,82 +121,47 @@ export const NavBar = () => {
           </Link>
         )}
 
-        {/* TODO: move animation to Menu component itself? */}
         {user ? (
-          <Menu>
-            {({ open }) => (
-              <>
-                <Menu.Button>
-                  <Avatar name={user.displayName} src={user.image} />
-                </Menu.Button>
-
-                <Transition
-                  as="div"
-                  show={open}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
+          <DropdownMenu>
+            <DropdownMenu.Trigger className="rounded-full">
+              <Avatar name={user.displayName} src={user.image} />
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Portal>
+              <DropdownMenu.Content align="end">
+                <DropdownMenu.Item onClick={() => event('NAV_profile')} asChild>
+                  <Link href={`/${user.customId ?? user.id}`}>
+                    <Avatar
+                      size="md"
+                      name={user.displayName}
+                      src={user.image}
+                    />
+                    <div>
+                      <h5 className="font-medium">{user.displayName}</h5>
+                      <p className="m-0">{user.email ?? 'Unknown email'}</p>
+                    </div>
+                  </Link>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item onClick={() => event('NAV_profile')} asChild>
+                  <Link href={`/${user.customId ?? user.id}`}>
+                    <MdAccountCircle /> My page
+                  </Link>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item
+                  onClick={() => event('NAV_settings')}
+                  asChild
                 >
-                  <Menu.Items>
-                    <Menu.Item
-                      className="!p-0 focus:!bg-transparent"
-                      onClick={() => event('NAV_profile')}
-                    >
-                      <Link
-                        className="flex gap-2 px-4 py-2"
-                        href={`/${user.customId ?? user.id}`}
-                      >
-                        <Avatar
-                          size="md"
-                          name={user.displayName}
-                          src={user.image}
-                        />
-                        <div>
-                          <h5>{user.displayName}</h5>
-                          <p className="m-0">{user.email ?? 'Unknown email'}</p>
-                        </div>
-                      </Link>
-                    </Menu.Item>
-                    <Menu.Item
-                      className="!p-0"
-                      onClick={() => event('NAV_profile')}
-                    >
-                      <Link
-                        className="flex size-full flex-row gap-2 px-4 py-2"
-                        href={`/${user.customId ?? user.id}`}
-                      >
-                        <MdAccountCircle className="text-white" /> My page
-                      </Link>
-                    </Menu.Item>
-                    <Menu.Item
-                      className="!p-0"
-                      onClick={() => event('NAV_settings')}
-                    >
-                      <Link
-                        className="flex size-full flex-row gap-2 px-4 py-2"
-                        href="/settings/profile"
-                      >
-                        <MdManageAccounts className="text-white" /> Settings
-                      </Link>
-                    </Menu.Item>
-                    <hr className="mx-3 my-1 border-t-2 border-neutral-400/10" />
-                    <Menu.Item
-                      icon={<MdExitToApp />}
-                      onClick={() => {
-                        event('NAV_logout');
-                        handleLogOutClick();
-                      }}
-                    >
-                      Log out
-                    </Menu.Item>
-                  </Menu.Items>
-                </Transition>
-              </>
-            )}
-          </Menu>
+                  <Link href={`/settings`}>
+                    <MdSettings /> Settings
+                  </Link>
+                </DropdownMenu.Item>
+                <DropdownMenu.Separator />
+                <DropdownMenu.Item onClick={handleLogOutClick}>
+                  <MdExitToApp />
+                  Log out
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+            </DropdownMenu.Portal>
+          </DropdownMenu>
         ) : (
           <Button onClick={handleLogin} className="my-2">
             Log in
