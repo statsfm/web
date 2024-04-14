4823ea3b39ea0c13829fa989dead339c737e2aa8
Martijn-Faber <martijn@fabtronics.nl>
2024-04-14 16:26:08 +0200

    fix(ui/DropdownMenu): forward refs

--- src/components/ui/DropdownMenu/Content.tsx
+++ src/components/ui/DropdownMenu/Content.tsx
@@ -1,16 +1,16 @@
-import { PropsWithChildren } from 'react';
+import { ElementRef, PropsWithChildren, forwardRef } from 'react';
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu';
 
 type DropdownMenuContentProps =
   PropsWithChildren<DropdownMenuPrimitive.DropdownMenuContentProps>;
 
-export const Content = ({
-  children,
-  sideOffset = 10,
-  ...props
-}: DropdownMenuContentProps) => {
+export const Content = forwardRef<
+  ElementRef<typeof DropdownMenuPrimitive.Content>,
+  DropdownMenuContentProps
+>(({ children, sideOffset = 10, ...props }, forwardedRef) => {
   return (
     <DropdownMenuPrimitive.Content
+      ref={forwardedRef}
       className="min-w-56 rounded-xl bg-foreground py-2 shadow-xl z-50"
       sideOffset={sideOffset}
       {...props}
@@ -18,4 +18,4 @@ export const Content = ({
       {children}
     </DropdownMenuPrimitive.Content>
   );
-};
+});
--- src/components/ui/DropdownMenu/Trigger.tsx
+++ src/components/ui/DropdownMenu/Trigger.tsx
@@ -1,13 +1,16 @@
-import { PropsWithChildren } from 'react';
+import { ElementRef, PropsWithChildren, forwardRef } from 'react';
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu';
 
 type DropdownMenuTriggerProps =
   PropsWithChildren<DropdownMenuPrimitive.DropdownMenuTriggerProps>;
 
-export const Trigger = ({ children, ...props }: DropdownMenuTriggerProps) => {
+export const Trigger = forwardRef<
+  ElementRef<typeof DropdownMenuPrimitive.Trigger>,
+  DropdownMenuTriggerProps
+>(({ children, ...props }, forwardedRef) => {
   return (
-    <DropdownMenuPrimitive.Trigger {...props}>
+    <DropdownMenuPrimitive.Trigger ref={forwardedRef} {...props}>
       {children}
     </DropdownMenuPrimitive.Trigger>
   );
-};
+});
