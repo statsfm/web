179d8a84876203da7f23bb0172af30d69731d1c5
Martijn-Faber <martijn@fabtronics.nl>
2024-04-15 17:05:46 +0200

    fix(DropdownMenu): add disabled color

--- src/components/ui/DropdownMenu/DropdownMenu.tsx
+++ src/components/ui/DropdownMenu/DropdownMenu.tsx
@@ -70,7 +70,7 @@ export const Item = forwardRef<
   return (
     <DropdownMenuPrimitive.Item
       ref={forwardedRef}
-      className="group leading-none px-4 py-3 rounded-md gap-2 flex items-center font-medium relative outline-none data-[disabled]:pointer-events-none data-[highlighted]:ring-2 data-[highlighted]:ring-neutral-500 data-[highlighted]:bg-background data-[highlighted]:cursor-pointer"
+      className="group leading-none px-4 py-3 rounded-md gap-2 flex items-center font-medium relative outline-none data-[disabled]:pointer-events-none data-[highlighted]:ring-2 data-[highlighted]:ring-neutral-500 data-[highlighted]:bg-background data-[highlighted]:cursor-pointer data-[disabled]:text-neutral-400"
       {...props}
     >
       {children}
