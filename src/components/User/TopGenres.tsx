c44d175b16c126b3cc0d2a32996a5da97107d577
Caleb Rortvedt <caleb@design.garden>
2024-11-19 17:48:54 -0600

    remove section from component

--- src/components/User/TopGenres.tsx
+++ src/components/User/TopGenres.tsx
@@ -6,9 +6,8 @@ import { useEffect, type FC, useState } from 'react';
 import { event } from 'nextjs-google-analytics';
 import { ChipGroup, Chip } from '../Chip';
 import Scope from '../PrivacyScope';
-import { Section } from '../Section';
 import type { TimeframeSelection } from './utils';
-import { getTimeframeOptions, getTimeframeText } from './utils';
+import { getTimeframeOptions } from './utils';
 import { NotEnoughData } from './NotEnoughData';
 
 export const TopGenres: FC<{
@@ -36,14 +35,7 @@ export const TopGenres: FC<{
   const isCurrentUser = currentUser?.id === userProfile.id;
 
   return (
-    <Section
-      title="Top genres"
-      description={`${
-        isCurrentUser ? 'Your' : `${userProfile.displayName}'s`
-      } top genres ${getTimeframeText(timeframe)}`}
-      scope="topGenres"
-      ref={ref}
-    >
+    <>
       <Scope value="topGenres">
         <ChipGroup
           className={clsx(topGenres.length === 0 && '!overflow-x-hidden')}
@@ -51,30 +43,30 @@ export const TopGenres: FC<{
           <NotEnoughData data={topGenres} loading={loading}>
             {topGenres?.length > 0
               ? topGenres.map((genre, i) => (
-                  <Chip key={i}>
-                    <Link
-                      legacyBehavior
-                      href={`/genre/${encodeURIComponent(genre.genre.tag)}`}
-                    >
-                      <a onClick={() => event('USER_top_genre_click')}>
-                        {genre.genre.tag}
-                      </a>
-                    </Link>
-                  </Chip>
-                ))
+                <Chip key={i}>
+                  <Link
+                    legacyBehavior
+                    href={`/genre/${encodeURIComponent(genre.genre.tag)}`}
+                  >
+                    <a onClick={() => event('USER_top_genre_click')}>
+                      {genre.genre.tag}
+                    </a>
+                  </Link>
+                </Chip>
+              ))
               : Array(8)
-                  .fill(null)
-                  .map((_v, i) => (
-                    <Chip
-                      className="shrink-0 animate-pulse !text-transparent"
-                      key={i}
-                    >
-                      {i.toString().repeat(i + (10 % 17))}
-                    </Chip>
-                  ))}
+                .fill(null)
+                .map((_v, i) => (
+                  <Chip
+                    className="shrink-0 animate-pulse !text-transparent"
+                    key={i}
+                  >
+                    {i.toString().repeat(i + (10 % 17))}
+                  </Chip>
+                ))}
           </NotEnoughData>
         </ChipGroup>
       </Scope>
-    </Section>
+    </>
   );
 };
