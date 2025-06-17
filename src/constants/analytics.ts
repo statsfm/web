0e1070d859e5bdd81bb9cf0be916190e736d1f1e
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2025-06-17 18:05:46 +0400

    chore(analytics): implement google analytics

--- src/constants/analytics.ts
+++ src/constants/analytics.ts
@@ -4,4 +4,8 @@ export const ANALYTICS_EVENTS = {
     CTA_CLICKED: 'referral_page_cta_clicked',
     PAGE_VIEWD: 'referral_child_referral_page_viewed',
   },
+  REVENUE: {
+    UNLOCK_CLICKED: 'web_revenue_unlock_plus_click',
+    STRIPE_SUCCESS: 'web_revenue_stripe_transaction_successfull_system',
+  },
 } as const;
--- src/hooks/use-analytics.ts
+++ src/hooks/use-analytics.ts
@@ -1,11 +1,18 @@
 import { useEffect, useState } from 'react';
 import { RudderAnalytics } from '@rudderstack/analytics-js';
 
-const useAnalytics = (): RudderAnalytics | undefined => {
+const hasEnv =
+  process.env.NEXT_PUBLIC_RUDDERSTACK_KEY &&
+  process.env.NEXT_PUBLIC_RUDDERSTACK_DATA_PLANE_URL;
+
+const useAnalytics = (): {
+  rudder: RudderAnalytics | undefined;
+  googleAnalytics: (name: string, value?: string) => void | undefined;
+} => {
   const [analytics, setAnalytics] = useState<RudderAnalytics>();
 
   useEffect(() => {
-    if (!analytics) {
+    if (!analytics && hasEnv) {
       const analyticsInstance = new RudderAnalytics();
       analyticsInstance.load(
         process.env.NEXT_PUBLIC_RUDDERSTACK_KEY,
@@ -15,7 +22,12 @@ const useAnalytics = (): RudderAnalytics | undefined => {
     }
   }, [analytics]);
 
-  return analytics;
+  return {
+    rudder: analytics,
+    googleAnalytics: (name: string, value?: string) => {
+      (window as any)?.gtag?.('event', name, { value: value ?? 'success' });
+    },
+  };
 };
 
 export default useAnalytics;
